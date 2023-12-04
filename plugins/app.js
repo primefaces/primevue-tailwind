7ff9cc4564d93bb0cebf775c91c6ec89147301f5
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-04 21:19:55 +0300

    Update app.js

--- plugins/app.js
+++ plugins/app.js
@@ -23,7 +23,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, {
         ripple: true,
         unstyled: true,
-        pt: TailwindUI
+        pt: Lara
     });
     nuxtApp.vueApp.directive('code', CodeHighlight);
     nuxtApp.vueApp.use($appStatePlugin);
