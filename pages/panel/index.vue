127bbd88d8ec93460e4253f001eefe39f552bfec
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-04 20:17:41 +0300

    Add Panel Docs

---
+++ pages/panel/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <DocComponent title="Vue Panel Component" header="Panel" description="Panel is a grouping component providing with content toggle feature." :componentDocs="docs" :presetDoc="presetDoc" apiLink="/inputtext/#api" ptLink="/inputtext/#pt" />
+</template>
+
+<script>
+import AccessibilityDoc from '@/doc/panel/AccessibilityDoc.vue';
+import BasicDoc from '@/doc/panel/BasicDoc.vue';
+import ImportDoc from '@/doc/panel/ImportDoc.vue';
+import TemplateDoc from '@/doc/panel/TemplateDoc.vue';
+import ToggleableDoc from '@/doc/panel/ToggleableDoc.vue';
+import PresetDoc from '@/doc/panel/preset/index.vue';
+export default {
+    data() {
+        return {
+            docs: [
+                {
+                    id: 'import',
+                    label: 'Import',
+                    component: ImportDoc
+                },
+                {
+                    id: 'basic',
+                    label: 'Basic',
+                    component: BasicDoc
+                },
+                {
+                    id: 'toggleable',
+                    label: 'Toggleable',
+                    component: ToggleableDoc
+                },
+                {
+                    id: 'template',
+                    label: 'Template',
+                    component: TemplateDoc
+                },
+                {
+                    id: 'accessibility',
+                    label: 'Accessibility',
+                    component: AccessibilityDoc
+                }
+            ],
+            presetDoc: PresetDoc
+        };
+    }
+};
+</script>
