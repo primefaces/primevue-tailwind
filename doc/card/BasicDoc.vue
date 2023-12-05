83667cf090a606bfc3b63176431ab3915a7bce80
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-05 14:09:07 +0300

    update card docs

--- doc/card/BasicDoc.vue
+++ doc/card/BasicDoc.vue
@@ -1,12 +1,12 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p class="!mb-0">A simple Card is created with a <i>title</i> property along with the content as children.</p>
+        <p>A simple Card is created with a <i>title</i> property along with the content as children.</p>
     </DocSectionText>
-    <div class="card flex items-center justify-center">
-        <Card class="card max-w-3xl">
+    <div class="card">
+        <Card>
             <template #title> Simple Card </template>
             <template #content>
-                <p class="!mb-0">
+                <p class="m-0">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
                     quas!
                 </p>
@@ -22,22 +22,23 @@ export default {
         return {
             code: {
                 basic: `
-<Card class="card max-w-3xl">
+<Card>
     <template #title> Simple Card </template>
     <template #content>
-        <p class="!mb-0">
+        <p class="m-0">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
             quas!
         </p>
     </template>
-</Card>`,
+</Card>
+`,
                 options: `
 <template>
-    <div class="card flex items-center justify-center">
-        <Card class="card max-w-3xl">
+    <div class="card">
+        <Card>
             <template #title> Simple Card </template>
             <template #content>
-                <p class="!mb-0">
+                <p class="m-0">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
                     quas!
                 </p>
@@ -47,14 +48,15 @@ export default {
 </template>
 
 <script>
-<\/script>`,
+<\/script>
+`,
                 composition: `
 <template>
-    <div class="card flex items-center justify-center">
-        <Card class="card max-w-3xl">
+    <div class="card">
+        <Card>
             <template #title> Simple Card </template>
             <template #content>
-                <p class="!mb-0">
+                <p class="m-0">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
                     quas!
                 </p>
@@ -64,7 +66,8 @@ export default {
 </template>
 
 <script setup>
-<\/script>`
+<\/script>
+`
             }
         };
     }
