c90e7bcebccff332c129ed407f0698d1a6aab0d4
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-05 17:53:20 +0300

    Finalize tag's preset (tw-ui)

--- presets/tailwindui/tag/index.js
+++ presets/tailwindui/tag/index.js
@@ -1,22 +1,33 @@
 export default {
     root: ({ props }) => ({
         class: [
+            //Font
+            'text-xs font-bold',
+
+            //Alignments
             'inline-flex items-center justify-center',
-            'bg-primary-500 text-white text-xs font-semibold px-2 py-1 ',
-            {
-                'bg-green-500 ': props.severity == 'success',
-                'bg-blue-500 ': props.severity == 'info',
-                'bg-orange-500 ': props.severity == 'warning',
-                'bg-red-500 ': props.severity == 'danger'
-            },
+
+            //Spacing
+            'px-2 py-1',
+
+            //Shape
             {
                 'rounded-md': !props.rounded,
                 'rounded-full': props.rounded
+            },
+
+            //Colors
+            'bg-primary-500 dark:bg-primary-400 text-white dark:text-surface-900',
+            {
+                'bg-green-500 dark:bg-green-400': props.severity == 'success',
+                'bg-blue-500 dark:bg-blue-400': props.severity == 'info',
+                'bg-orange-500 dark:bg-orange-400': props.severity == 'warning',
+                'bg-red-500 dark:bg-red-400': props.severity == 'danger'
             }
         ]
     }),
     value: {
-        class: 'leading-6'
+        class: 'leading-normal'
     },
     icon: {
         class: 'mr-1 text-sm'
