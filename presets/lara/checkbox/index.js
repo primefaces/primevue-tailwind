1d6893bdf38cf4ce90945ff0e8139000059ac57e
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-06 16:36:12 +0300

    Finalize checkbox's preset (lara)

--- presets/lara/checkbox/index.js
+++ presets/lara/checkbox/index.js
@@ -1,22 +1,71 @@
 export default {
     root: {
-        class: ['cursor-pointer inline-flex relative select-none align-bottom', 'w-6 h-6']
+        class: [
+            'relative',
+
+            // Alignment
+            'inline-flex',
+            'align-bottom',
+
+            // Size
+            'w-6',
+            'h-6',
+
+            // Misc
+            'cursor-pointer',
+            'select-none'
+        ]
     },
     input: ({ props, context }) => ({
         class: [
-            'flex items-center justify-center',
-            'border-2 w-6 h-6 text-surface-600 rounded-lg transition-colors duration-200',
+            // Alignment
+            'flex',
+            'items-center',
+            'justify-center',
+
+            // Size
+            'w-6',
+            'h-6',
+
+            // Shape
+            'rounded-lg',
+            'border-2',
+
+            // Colors
+            'text-surface-600',
             {
                 'border-surface-200 bg-surface-0 dark:border-surface-700 dark:bg-surface-900': !context.checked,
                 'border-primary-500 bg-primary-500 dark:border-primary-400 dark:bg-primary-400': context.checked
             },
+
+            // States
+            'focus:outline-none focus:outline-offset-0',
             {
-                'hover:border-primary-500 dark:hover:border-primary-400 focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 ring-inset dark:focus:ring-primary-300/50': !props.disabled,
+                'hover:border-primary-500 dark:hover:border-primary-400': !props.disabled,
+                'focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50': !props.disabled,
                 'cursor-default opacity-60': props.disabled
-            }
+            },
+
+            // Transitions
+            'transition-colors',
+            'duration-200'
         ]
     }),
     icon: {
-        class: 'w-4 h-4 transition-all duration-200 text-white text-base dark:text-surface-900'
+        class: [
+            // Font
+            'text-normal',
+
+            // Size
+            'w-4',
+            'h-4',
+
+            // Colors
+            'text-white dark:text-surface-900',
+
+            // Transitions
+            'transition-all',
+            'duration-200'
+        ]
     }
 };
