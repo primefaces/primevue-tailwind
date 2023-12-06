aedb145fee8e8e3c9b5cc4530cd4a264cf4031f3
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-06 13:21:26 +0300

    Refactor inputswitch's preset (lara)

--- presets/lara/inputswitch/index.js
+++ presets/lara/inputswitch/index.js
@@ -5,19 +5,42 @@ export default {
             'w-12 h-7',
             {
                 'opacity-60 select-none pointer-events-none cursor-default': props.disabled
-            }
+            },
+            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50  dark:focus:ring-primary-300/50'
         ]
     }),
     slider: ({ props }) => ({
         class: [
-            'absolute cursor-pointer top-0 left-0 right-0 bottom-0 border border-transparent',
-            'transition-colors duration-200 rounded-2xl',
-            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 ring-inset dark:focus:ring-primary-300/50',
-            "before:absolute before:content-'' before:top-1/2 before:bg-surface-0 before:dark:bg-surface-900 before:w-5 before:h-5 before:left-1 before:-mt-2.5 before:rounded-full before:transition-duration-200",
+            // Position
+            'absolute top-0 left-0 right-0 bottom-0',
+            { 'before:transform before:translate-x-5': props.modelValue },
+
+            // Shape
+            'rounded-2xl',
+
+            // Before:
+            'before:absolute before:top-1/2 before:left-1',
+            'before:-mt-2.5',
+            'before:h-5 before:w-5',
+            'before:rounded-full',
+            'before:transition-duration-200',
+            'before:bg-surface-0 before:dark:bg-surface-900',
+
+            // Colors
+            'border border-transparent',
             {
-                'bg-surface-200 dark:bg-surface-800 hover:bg-surface-300 hover:dark:bg-surface-700 ': !props.modelValue,
-                'bg-primary-500 before:transform before:translate-x-5': props.modelValue
-            }
+                'bg-surface-200 dark:bg-surface-700': !props.modelValue,
+                'bg-primary-500 dark:bg-primary-400': props.modelValue
+            },
+
+            // States
+            { 'hover:bg-surface-300 hover:dark:bg-surface-600 ': !props.modelValue },
+
+            // Transition
+            'transition-colors duration-200',
+
+            // Misc
+            'cursor-pointer'
         ]
     })
 };
