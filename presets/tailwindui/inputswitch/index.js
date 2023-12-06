5e25a9aeaea4f07d88fcc1235f6f78216089ea11
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-06 13:34:57 +0300

    Finalize inputswitch's preset (tw-ui)

--- presets/tailwindui/inputswitch/index.js
+++ presets/tailwindui/inputswitch/index.js
@@ -1,8 +1,15 @@
 export default {
     root: ({ props }) => ({
         class: [
-            'inline-block relative',
-            'w-12 h-7',
+            // Alignments
+            'inline-flex relative',
+            'flex-shrink-0',
+
+            // Size
+            'h-5 w-9',
+
+            // States
+            'focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 focus-visible:ring-offset-2 focus-visible:ring-offset-white dark:focus-visible:ring-offset-gray-900',
             {
                 'opacity-60 select-none pointer-events-none cursor-default': props.disabled
             }
@@ -10,14 +17,37 @@ export default {
     }),
     slider: ({ props }) => ({
         class: [
-            'absolute cursor-pointer top-0 left-0 right-0 bottom-0 border border-transparent',
-            'transition-colors duration-200 rounded-2xl',
-            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 ring-inset dark:focus:ring-primary-300/50',
-            "before:absolute before:content-'' before:top-1/2 before:bg-surface-0 before:dark:bg-surface-900 before:w-5 before:h-5 before:left-1 before:-mt-2.5 before:rounded-full before:transition-duration-200",
+            // Position
+            'absolute top-0 left-0 right-0 bottom-0',
+
+            // Shape
+            'rounded-2xl',
+
+            // Before:
+            'before:absolute before:top-1/2',
+            'before:-mt-2',
+            'before:h-4 before:w-4',
+            'before:rounded-full',
+            'before:transition-duration-200 before:transition before:ease-in-out',
+            'before:bg-surface-0 before:dark:bg-surface-900',
+            'before:shadow',
+            { 'before:transform before:translate-x-4': props.modelValue },
+
+            // Colors
+            'border-2 border-transparent',
             {
-                'bg-surface-200 dark:bg-surface-800 hover:bg-surface-300 hover:dark:bg-surface-700 ': !props.modelValue,
-                'bg-primary-500 before:transform before:translate-x-5': props.modelValue
-            }
+                'bg-surface-200 dark:bg-surface-700': !props.modelValue,
+                'bg-primary-500 dark:bg-primary-400': props.modelValue
+            },
+
+            // States
+            { 'hover:bg-surface-300 hover:dark:bg-surface-600 ': !props.modelValue },
+
+            // Transition
+            'transition-colors duration-200',
+
+            // Misc
+            'cursor-pointer'
         ]
     })
 };
