be59207d41b995390d001f9cf3ec68813a700362
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-07 10:52:36 +0300

    add calendar docs & refactor calendar preset (lara)

---
+++ doc/calendar/IconDoc.vue
@@ -0,0 +1,111 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>An additional button is displayed next to the input field when <i>showIcon</i> is present. Also adding <i>iconDisplay</i> as <i>input</i> an icon will be displayed.</p>
+    </DocSectionText>
+    <div class="card flex flex-wrap gap-3 p-fluid">
+        <div class="flex-auto">
+            <label for="buttondisplay" class="font-bold block mb-2"> Button Display </label>
+            <Calendar v-model="buttondisplay" showIcon inputId="buttondisplay" />
+        </div>
+        <div class="flex-auto">
+            <label for="icondisplay" class="font-bold block mb-2"> Icon Display </label>
+            <Calendar v-model="icondisplay" showIcon iconDisplay="input" inputId="icondisplay" />
+        </div>
+        <div class="flex-auto">
+            <label for="templatedisplay" class="font-bold block mb-2"> Icon Template </label>
+            <Calendar v-model="templatedisplay" showIcon iconDisplay="input" timeOnly inputId="templatedisplay">
+                <template #inputicon="{ clickCallback }">
+                    <i class="pi pi-clock" @click="clickCallback" />
+                </template>
+            </Calendar>
+        </div>
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            buttondisplay: null,
+            icondisplay: null,
+            templatedisplay: null,
+            code: {
+                basic: `
+<Calendar v-model="buttondisplay" showIcon />
+<Calendar v-model="icondisplay" showIcon iconDisplay="input" />
+<Calendar v-model="templatedisplay" showIcon iconDisplay="input" timeOnly>
+    <template #inputicon="{ clickCallback }">
+        <i class="pi pi-clock" @click="clickCallback" />
+    </template>
+</Calendar>
+`,
+                options: `
+<template>
+    <div class="card flex flex-wrap gap-3 p-fluid">
+        <div class="flex-auto">
+            <label for="buttondisplay" class="font-bold block mb-2"> Button Display </label>
+            <Calendar v-model="buttondisplay" showIcon inputId="buttondisplay" />
+        </div>
+        <div class="flex-auto">
+            <label for="icondisplay" class="font-bold block mb-2"> Icon Display </label>
+            <Calendar v-model="icondisplay" showIcon iconDisplay="input" inputId="icondisplay" />
+        </div>
+        <div class="flex-auto">
+            <label for="templatedisplay" class="font-bold block mb-2"> Icon Template </label>
+            <Calendar v-model="templatedisplay" showIcon iconDisplay="input" timeOnly inputId="templatedisplay">
+                <template #inputicon="{ clickCallback }">
+                    <i class="pi pi-clock" @click="clickCallback" />
+                </template>
+            </Calendar>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            buttondisplay: null,
+            icondisplay: null,
+            templatedisplay: null,
+        };
+    }
+};
+<\/script>
+`,
+                composition: `
+<template>
+    <div class="card flex flex-wrap gap-3 p-fluid">
+        <div class="flex-auto">
+            <label for="buttondisplay" class="font-bold block mb-2"> Button Display </label>
+            <Calendar v-model="buttondisplay" showIcon inputId="buttondisplay" />
+        </div>
+        <div class="flex-auto">
+            <label for="icondisplay" class="font-bold block mb-2"> Icon Display </label>
+            <Calendar v-model="icondisplay" showIcon iconDisplay="input" inputId="icondisplay" />
+        </div>
+        <div class="flex-auto">
+            <label for="templatedisplay" class="font-bold block mb-2"> Icon Template </label>
+            <Calendar v-model="templatedisplay" showIcon iconDisplay="input" timeOnly inputId="templatedisplay">
+                <template #inputicon="{ clickCallback }">
+                    <i class="pi pi-clock" @click="clickCallback" />
+                </template>
+            </Calendar>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const buttondisplay = ref();
+const icondisplay = ref();
+const templatedisplay = ref();
+<\/script>
+`
+            }
+        };
+    }
+};
+</script>
--- presets/lara/calendar/index.js
+++ presets/lara/calendar/index.js
@@ -1,22 +1,50 @@
 export default {
     root: ({ props }) => ({
         class: [
-            'inline-flex max-w-full relative',
-            {
-                'opacity-60 select-none pointer-events-none cursor-default': props.disabled
-            }
+            // Display and Position
+            'inline-flex',
+            'max-w-full',
+            'relative',
+
+            // Misc
+            { 'opacity-60 select-none pointer-events-none cursor-default': props.disabled }
         ]
     }),
     input: {
         class: [
-            'font-sans text-base text-surface-600 dark:text-white/80 bg-surface-0 dark:bg-surface-900 p-3 border border-surface-200 dark:border-surface-700 transition-colors duration-200 appearance-none rounded-lg',
-            'hover:border-primary-500' //Hover
+            // Font
+            'font-sans',
+            'text-base',
+            'text-surface-600 dark:text-white/80',
+
+            // Colors
+            'bg-surface-0 dark:bg-surface-900',
+            'border border-surface-200 dark:border-surface-700',
+
+            // Spacing
+            'p-3',
+
+            // Shape
+            'rounded-lg',
+            'appearance-none',
+
+            // Transitions
+            'transition-colors',
+            'duration-200',
+
+            // States
+            'hover:border-primary-500' // Hover
         ]
     },
     panel: ({ props }) => ({
         class: [
+            // Colors
             'bg-surface-0 dark:bg-surface-900',
+
+            // Size
             'min-w-[350px]',
+
+            // Shape and Misc
             {
                 'shadow-md border-0 absolute': !props.inline,
                 'inline-block overflow-x-auto border border-surface-200 dark:border-surface-700 p-2 rounded-lg': props.inline
@@ -24,123 +52,344 @@ export default {
         ]
     }),
     header: {
-        class: ['flex items-center justify-between', 'p-2 text-surface-700 dark:text-white/80 bg-surface-0 dark:bg-surface-900 font-semibold m-0 border-b border-surface-200 dark:border-surface-700 rounded-t-lg']
+        class: [
+            // Flexbox and Alignment
+            'flex items-center justify-between',
+
+            // Spacing
+            'p-2',
+            'm-0',
+
+            // Colors
+            'text-surface-700 dark:text-white/80',
+            'bg-surface-0 dark:bg-surface-900',
+
+            // Shape
+            'border-b',
+            'border-surface-200 dark:border-surface-700',
+            'rounded-t-lg'
+        ]
     },
     previousbutton: {
         class: [
-            'flex items-center justify-center cursor-pointer overflow-hidden relative',
-            'w-8 h-8 text-surface-600 dark:text-white/70 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
-            'hover:text-surface-700 dark:hover:text-white/80 hover:border-transparent hover:bg-surface-100 dark:hover:bg-surface-800/80 '
+            // Flexbox and Alignment
+            'flex items-center justify-center',
+
+            // Size and Shape
+            'w-8 h-8',
+            'rounded-full',
+
+            // Colors
+            'text-surface-600 dark:text-white/70',
+            'border-0',
+            'bg-transparent',
+
+            // Transitions
+            'transition-colors duration-200 ease-in-out',
+
+            // States
+            'hover:text-surface-700 dark:hover:text-white/80',
+            'hover:border-transparent',
+            'hover:bg-surface-100 dark:hover:bg-surface-800/80'
         ]
     },
     title: {
-        class: 'leading-8 mx-auto'
+        class: [
+            // Text
+            'leading-8',
+            'mx-auto'
+        ]
     },
     monthTitle: {
-        class: ['text-surface-700 dark:text-white/80 transition duration-200 font-semibold p-2', 'mr-2', 'hover:text-primary-500']
+        class: [
+            // Font and Colors
+            'text-surface-700 dark:text-white/80',
+            'font-semibold',
+            'transition duration-200',
+
+            // Spacing
+            'p-2',
+            'mr-2',
+
+            // States
+            'hover:text-primary-500'
+        ]
     },
     yearTitle: {
-        class: ['text-surface-700 dark:text-white/80 transition duration-200 font-semibold p-2', 'hover:text-primary-500']
+        class: [
+            // Font and Colors
+            'text-surface-700 dark:text-white/80',
+            'font-semibold',
+            'transition duration-200',
+
+            // Spacing
+            'p-2',
+
+            // States
+            'hover:text-primary-500'
+        ]
     },
     nextbutton: {
         class: [
-            'flex items-center justify-center cursor-pointer overflow-hidden relative',
-            'w-8 h-8 text-surface-600 dark:text-white/70 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
-            'hover:text-surface-700 dark:hover:text-white/80 hover:border-transparent hover:bg-surface-100 dark:hover:bg-surface-800/80 '
+            // Flexbox and Alignment
+            'flex items-center justify-center',
+
+            // Size and Shape
+            'w-8 h-8',
+            'rounded-full',
+
+            // Colors
+            'text-surface-600 dark:text-white/70',
+            'border-0',
+            'bg-transparent',
+
+            // Transitions
+            'transition-colors duration-200 ease-in-out',
+
+            // States
+            'hover:text-surface-700 dark:hover:text-white/80',
+            'hover:border-transparent',
+            'hover:bg-surface-100 dark:hover:bg-surface-800/80'
         ]
     },
     table: {
-        class: ['border-collapse w-full', 'my-2']
+        class: [
+            // Table
+            'border-collapse',
+            'w-full',
+
+            // Spacing
+            'my-2'
+        ]
     },
     tableheadercell: {
-        class: 'p-2'
+        class: [
+            // Spacing
+            'p-2'
+        ]
     },
     weekday: {
-        class: 'text-surface-600 dark:text-white/70'
+        class: [
+            // Colors
+            'text-surface-600 dark:text-white/70'
+        ]
     },
     day: {
-        class: 'p-2'
+        class: [
+            // Spacing
+            'p-2'
+        ]
     },
     daylabel: ({ context }) => ({
         class: [
-            'w-10 h-10 rounded-full transition-shadow duration-200 border-transparent border',
-            'flex items-center justify-center mx-auto overflow-hidden relative',
+            // Shape
+            'w-10 h-10',
+            'rounded-full',
+            'border-transparent border',
+
+            // Flexbox and Alignment
+            'flex items-center justify-center',
+            'mx-auto',
+
+            // States
             'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50',
             {
                 'opacity-60 cursor-default': context.disabled,
                 'cursor-pointer': !context.disabled
             },
             {
-                'text-surface-600 dark:text-white/70 bg-transprent hover:bg-surface-100 dark:hover:bg-surface-800/80': !context.selected && !context.disabled,
+                'text-surface-600 dark:text-white/70 bg-transparent hover:bg-surface-100 dark:hover:bg-surface-800/80': !context.selected && !context.disabled,
                 'text-primary-700 bg-primary-100 hover:bg-primary-200': context.selected && !context.disabled
             }
         ]
     }),
     monthpicker: {
-        class: 'my-2'
+        class: [
+            // Spacing
+            'my-2'
+        ]
     },
     month: ({ context }) => ({
         class: [
-            'w-1/3 inline-flex items-center justify-center cursor-pointer overflow-hidden relative',
-            'p-2 transition-shadow duration-200 rounded-lg',
+            // Flexbox and Alignment
+            'w-1/3',
+            'inline-flex items-center justify-center',
+
+            // Shape
+            'p-2',
+            'rounded-lg',
+
+            // States
             'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50',
             {
-                'text-surface-600 dark:text-white/70 bg-transprent hover:bg-surface-100 dark:hover:bg-surface-800/80': !context.selected && !context.disabled,
+                'text-surface-600 dark:text-white/70 bg-transparent hover:bg-surface-100 dark:hover:bg-surface-800/80': !context.selected && !context.disabled,
                 'text-primary-700 bg-primary-100 hover:bg-primary-200': context.selected && !context.disabled
             }
         ]
     }),
     yearpicker: {
-        class: 'my-2'
+        class: [
+            // Spacing
+            'my-2'
+        ]
     },
     year: ({ context }) => ({
         class: [
-            'w-1/2 inline-flex items-center justify-center cursor-pointer overflow-hidden relative',
-            'p-2 transition-shadow duration-200 rounded-lg',
+            // Flexbox and Alignment
+            'w-1/2',
+            'inline-flex items-center justify-center',
+
+            // Shape
+            'p-2',
+            'rounded-lg',
+
+            // States
             'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50',
             {
-                'text-surface-600 dark:text-white/70 bg-transprent hover:bg-surface-100 dark:hover:bg-surface-800/80': !context.selected && !context.disabled,
+                'text-surface-600 dark:text-white/70 bg-transparent hover:bg-surface-100 dark:hover:bg-surface-800/80': !context.selected && !context.disabled,
                 'text-primary-700 bg-primary-100 hover:bg-primary-200': context.selected && !context.disabled
             }
         ]
     }),
     timepicker: {
-        class: ['flex justify-center items-center', 'border-t-1 border-solid border-surface-200 p-2']
+        class: [
+            // Flexbox
+            'flex',
+            'justify-center items-center',
+
+            // Borders
+            'border-t-1',
+            'border-solid border-surface-200',
+
+            // Spacing
+            'p-2'
+        ]
     },
     separatorcontainer: {
-        class: 'flex items-center flex-col px-2'
+        class: [
+            // Flexbox and Alignment
+            'flex',
+            'items-center',
+            'flex-col',
+
+            // Spacing
+            'px-2'
+        ]
     },
     separator: {
-        class: 'text-xl'
+        class: [
+            // Text
+            'text-xl'
+        ]
     },
     hourpicker: {
-        class: 'flex items-center flex-col px-2'
+        class: [
+            // Flexbox and Alignment
+            'flex',
+            'items-center',
+            'flex-col',
+
+            // Spacing
+            'px-2'
+        ]
     },
     minutepicker: {
-        class: 'flex items-center flex-col px-2'
+        class: [
+            // Flexbox and Alignment
+            'flex',
+            'items-center',
+            'flex-col',
+
+            // Spacing
+            'px-2'
+        ]
     },
     ampmpicker: {
-        class: 'flex items-center flex-col px-2'
+        class: [
+            // Flexbox and Alignment
+            'flex',
+            'items-center',
+            'flex-col',
+
+            // Spacing
+            'px-2'
+        ]
     },
     incrementbutton: {
         class: [
-            'flex items-center justify-center cursor-pointer overflow-hidden relative',
-            'w-8 h-8 text-surface-600 dark:text-white/70 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
-            'hover:text-surface-700 dark:hover:text-white/80 hover:border-transparent hover:bg-surface-100 dark:hover:bg-surface-800/80 '
+            // Flexbox and Alignment
+            'flex',
+            'items-center',
+            'justify-center',
+
+            // Size and Shape
+            'w-8 h-8',
+            'rounded-full',
+
+            // Colors
+            'text-surface-600 dark:text-white/70',
+            'border-0',
+            'bg-transparent',
+
+            // Transitions
+            'transition-colors duration-200 ease-in-out',
+
+            // States
+            'hover:text-surface-700 dark:hover:text-white/80',
+            'hover:border-transparent',
+            'hover:bg-surface-100 dark:hover:bg-surface-800/80'
         ]
     },
     decrementbutton: {
         class: [
-            'flex items-center justify-center cursor-pointer overflow-hidden relative',
-            'w-8 h-8 text-surface-600 dark:text-white/70 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
-            'hover:text-surface-700 dark:hover:text-white/80 hover:border-transparent hover:bg-surface-100 dark:hover:bg-surface-800/80 '
+            // Flexbox and Alignment
+            'flex',
+            'items-center',
+            'justify-center',
+
+            // Size and Shape
+            'w-8 h-8',
+            'rounded-full',
+
+            // Colors
+            'text-surface-600 dark:text-white/70',
+            'border-0',
+            'bg-transparent',
+
+            // Transitions
+            'transition-colors duration-200 ease-in-out',
+
+            // States
+            'hover:text-surface-700 dark:hover:text-white/80',
+            'hover:border-transparent',
+            'hover:bg-surface-100 dark:hover:bg-surface-800/80'
         ]
     },
     groupcontainer: {
-        class: 'flex'
+        class: [
+            // Flexbox
+            'flex'
+        ]
     },
     group: {
-        class: ['flex-1', 'border-l border-surface-200 pr-0.5 pl-0.5 pt-0 pb-0', 'first:pl-0 first:border-l-0']
+        class: [
+            // Flexbox and Sizing
+            'flex-1',
+
+            // Borders
+            'border-l',
+            'border-surface-200',
+
+            // Spacing
+            'pr-0.5',
+            'pl-0.5',
+            'pt-0',
+            'pb-0',
+
+            // Pseudo-Classes
+            'first:pl-0',
+            'first:border-l-0'
+        ]
     },
     transition: {
         enterFromClass: 'opacity-0 scale-75',
