9ef4f728eb44dfdd37229f966b2ed4e24ea941a9
Cagatay Civici <cagatay.civici@gmail.com>
2023-12-09 01:45:16 +0300

    Add links

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -3,8 +3,10 @@
         <div class="doc-intro">
             <h1>Gallery</h1>
             <p>
-                A community-driven collection of customizable presets with Tailwind CSS for PrimeVue. Create, share, and discover unique designs. If you'd like contribute, please
-                <a href="#">submit</a> your own preset to be displayed here. It may be just for a single component or multiple, let's build it together.
+                A community-driven collection of customizable presets with Tailwind CSS for PrimeVue. Create, share and discover unique designs. If you'd like contribute, please build your design based on the
+                <a href="https://github.com/primefaces/primevue-tailwind-preset-template">custom preset template</a> project and then
+                <a href="https://github.com/primefaces/primevue-tailwind/issues/new?assignees=&labels=Status%3A+Needs+Triage&projects=&template=new_preset.yml&title=Preset+Name">submit</a> it to be displayed here. It may be just for a single
+                component or multiple.
             </p>
         </div>
 
