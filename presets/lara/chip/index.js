4ec5325b58d37aab94f401c4caab74ce936ac7c6
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-09 23:24:43 +0300

    finalize chip preset (lara)

--- presets/lara/chip/index.js
+++ presets/lara/chip/index.js
@@ -1,17 +1,45 @@
 export default {
     root: {
-        class: ['inline-flex items-center', 'bg-surface-200 text-surface-800 rounded-[16px] px-3 dark:text-white/80 dark:bg-surface-900']
+        class: [
+            // Flexbox
+            'inline-flex items-center',
+
+            // Spacing
+            'px-3',
+
+            // Shape
+            'rounded-[1.14rem]',
+
+            // Colors
+            'text-surface-700 dark:text-white/70',
+            'bg-surface-200 dark:bg-surface-700'
+        ]
     },
     label: {
-        class: 'leading-6 mt-1.5 mb-1.5'
+        class: 'leading-6 my-1.5 mx-0'
     },
     icon: {
         class: 'leading-6 mr-2'
     },
     image: {
-        class: ['w-9 h-9 ml-[-0.75rem] mr-2', 'rounded-full']
+        class: ['w-9 h-9 -ml-3 mr-2', 'rounded-full']
     },
     removeIcon: {
-        class: ['ml-2 rounded-md transition duration-200 ease-in-out', 'cursor-pointer leading-6']
+        class: [
+            // Shape
+            'rounded-md leading-6',
+
+            // Spacing
+            'ml-2',
+
+            // Size
+            'w-4 h-4',
+
+            // Transition
+            'transition duration-200 ease-in-out',
+
+            // Misc
+            'cursor-pointer'
+        ]
     }
 };
