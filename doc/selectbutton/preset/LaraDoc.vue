8a2fb07caf406a568f495c19dffb1ec61630e04e
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-09 15:53:46 +0300

    Add selectbutton docs

---
+++ doc/selectbutton/preset/LaraDoc.vue
@@ -0,0 +1,54 @@
+<template>
+    <DocSectionText v-bind="$attrs"></DocSectionText>
+    <DocSectionCode :code="code" hideToggleCode importCode hideCodeSandbox hideStackBlitz />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: null,
+            code: {
+                basic: `
+inputtext: {
+    root: ({ props, context }) => ({
+        class: [
+            // Font
+            'font-sans text-base leading-none',
+
+            // Sizing
+            'm-0',
+            {
+                'px-4 py-4': props.size == 'large',
+                'px-2 py-2': props.size == 'small',
+                'p-3': props.size == null
+            },
+
+            // Colors
+            'text-surface-600 dark:text-surface-200',
+            'placeholder:text-surface-400 dark:placeholder:text-surface-500',
+            'bg-surface-0 dark:bg-surface-900',
+            'border border-surface-300 dark:border-surface-700',
+
+            // Shape
+            'rounded-lg',
+            'appearance-none',
+
+            // Interactions
+            {
+                'hover:border-primary-500 dark:hover:border-primary-400': !context.disabled,
+                'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-500/50 dark:focus:ring-primary-400/50': !context.disabled,
+                'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+            },
+
+            // Transitions
+            'transition-colors duration-200'
+        ]
+    })
+}
+`
+            }
+        };
+    }
+};
+</script>
---
+++ pages/selectbutton/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <DocComponent title="Vue SelectButton Component" header="SelectButton" description="Slider is a component to provide input with a drag handle." :componentDocs="docs" :presetDoc="presetDoc" apiLink="/inputtext/#api" ptLink="/inputtext/#pt" />
+</template>
+
+<script>
+import PresetDoc from '@/doc/fieldset/preset/index.vue';
+import AccessibilityDoc from '@/doc/selectbutton/AccessibilityDoc.vue';
+import BasicDoc from '@/doc/selectbutton/BasicDoc.vue';
+import DisabledDoc from '@/doc/selectbutton/DisabledDoc.vue';
+import ImportDoc from '@/doc/selectbutton/ImportDoc.vue';
+import InvalidDoc from '@/doc/selectbutton/InvalidDoc.vue';
+import MultipleDoc from '@/doc/selectbutton/MultipleDoc.vue';
+import TemplateDoc from '@/doc/selectbutton/TemplateDoc.vue';
+
+export default {
+    data() {
+        return {
+            docs: [
+                {
+                    id: 'import',
+                    label: 'Import',
+                    component: ImportDoc
+                },
+                {
+                    id: 'basic',
+                    label: 'Basic',
+                    component: BasicDoc
+                },
+                {
+                    id: 'multiple',
+                    label: 'Multiple',
+                    component: MultipleDoc
+                },
+                {
+                    id: 'template',
+                    label: 'Template',
+                    component: TemplateDoc
+                },
+                {
+                    id: 'invalid',
+                    label: 'Invalid',
+                    component: InvalidDoc
+                },
+                {
+                    id: 'disabled',
+                    label: 'Disabled',
+                    component: DisabledDoc
+                },
+
+                {
+                    id: 'accessibility',
+                    label: 'Accessibility',
+                    component: AccessibilityDoc
+                }
+            ],
+            presetDoc: PresetDoc
+        };
+    }
+};
+</script>
