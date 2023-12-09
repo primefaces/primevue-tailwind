8952cea5c6c52ea4f7aa80c1288695b3ed00f836
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-09 16:19:19 +0300

    Add selectbutton's preset (tw-ui)

--- presets/tailwindui/selectbutton/index.js
+++ presets/tailwindui/selectbutton/index.js
@@ -1,22 +1,45 @@
 export default {
     root: ({ props }) => ({
-        class: [{ 'opacity-60 select-none pointer-events-none cursor-default': props.disabled }]
+        class: ['shadow-sm', { 'opacity-60 select-none pointer-events-none cursor-default': props.disabled }]
     }),
     button: ({ context }) => ({
         class: [
-            'inline-flex cursor-pointer select-none items-center align-bottom text-center overflow-hidden relative',
-            'px-4 py-3',
-            'transition duration-200 border border-r-0',
-            'first:rounded-l-md first:rounded-tr-none first:rounded-br-none last:border-r last:rounded-tl-none last:rounded-bl-none last:rounded-r-md',
-            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 ring-inset dark:focus:ring-primary-300/50',
+            'relative',
+            // Font
+            'text-sm',
+            'leading-none',
+
+            // Flex Alignment
+            'inline-flex items-center align-bottom text-center',
+
+            // Spacing
+            'px-2.5 py-1.5',
+
+            // Shape
+            'ring-1 ring-surface-200 dark:ring-surface-700',
+            'first:rounded-l-md first:rounded-tr-none first:rounded-br-none',
+            'last:rounded-tl-none last:rounded-bl-none last:rounded-r-md ',
+
+            // Color
             {
-                'bg-surface-0 dark:bg-surface-900 text-surface-700 dark:text-white/80 border-surface-200 dark:border-surface-700 hover:bg-surface-50 dark:hover:bg-surface-800/80 ': !context.active,
-                'bg-primary-500 border-primary-500 text-white hover:bg-primary-600': context.active,
-                'opacity-60 select-none pointer-events-none cursor-default': context.disabled
-            }
+                'bg-surface-0 dark:bg-surface-900': !context.active,
+                'text-surface-700 dark:text-white/80': !context.active,
+                'bg-surface-100 dark:bg-surface-700 border-primary-500 dark:border-primary-400': context.active
+            },
+
+            // States
+            'focus:outline-none focus:outline-offset-0 focus:ring-primary-500 dark:focus:ring-primary-400',
+            'hover:bg-surface-200 dark:hover:bg-surface-600/80',
+            { 'opacity-60 select-none pointer-events-none cursor-default': context.disabled },
+
+            // Transition
+            'transition duration-200',
+
+            // Misc
+            'cursor-pointer select-none overflow-hidden'
         ]
     }),
     label: {
-        class: 'font-bold'
+        class: 'font-semibold'
     }
 };
