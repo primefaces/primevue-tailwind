2e34c49c40916d841a3312504632b56a6aa4ead1
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-09 20:35:30 +0300

    Add sidebar's preset (tw-ui)

--- presets/tailwindui/sidebar/index.js
+++ presets/tailwindui/sidebar/index.js
@@ -1,36 +1,122 @@
 export default {
     root: ({ props }) => ({
         class: [
-            'flex flex-col pointer-events-auto relative transition-transform duration-300',
-            'bg-surface-0 text-surface-700 border-0 shadow-lg',
+            // Flexbox
+            'flex flex-col',
+
+            // Position
+            'relative',
+            { '!transition-none !transform-none !w-screen !h-screen !max-h-full !top-0 !left-0': props.position == 'full' },
+
+            // Size
             {
-                '!transition-none !transform-none !w-screen !h-screen !max-h-full !top-0 !left-0': props.position == 'full',
-                'h-full w-80': props.position == 'left' || props.position == 'right',
-                'h-40 w-full': props.position == 'top' || props.position == 'bottom'
+                'h-full w-full max-w-[28rem]': props.position == 'left' || props.position == 'right',
+                'h-auto w-full': props.position == 'top' || props.position == 'bottom'
             },
-            'dark:border dark:border-surface-700 dark:bg-surface-900 dark:text-white/80'
+
+            // Shape
+            'border-0 dark:border',
+            'shadow-xl',
+
+            // Colors
+            'bg-surface-0 dark:bg-surface-800',
+            'text-surface-700 dark:text-white/80',
+            'dark:border-surface-700',
+
+            // Transitions
+            'transition-transform',
+            'duration-300',
+
+            // Misc
+            'pointer-events-auto'
         ]
     }),
     header: {
-        class: ['flex items-center justify-end', 'p-5']
+        class: [
+            // Flexbox and Alignment
+            'flex items-center justify-between',
+            'shrink-0',
+
+            // Spacing
+            'px-6 pt-6',
+
+            // Colors
+            'bg-surface-0 dark:bg-surface-800',
+            'text-surface-700 dark:text-surface-0/80'
+        ]
+    },
+    title: {
+        class: ['font-semibold leading-6 text-base m-0']
+    },
+    icons: {
+        class: ['flex items-center']
     },
     closeButton: {
         class: [
-            'flex items-center justify-center overflow-hidden relative',
-            'w-8 h-8 text-surface-500 border-0 bg-transparent rounded-full transition duration-200 ease-in-out mr-2 last:mr-0',
-            'hover:text-surface-700 hover:border-transparent hover:bg-surface-100',
-            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50 ring-inset', // focus
-            'dark:hover:text-white/80 dark:hover:border-transparent dark:hover:bg-surface-800/80'
+            'relative',
+
+            // Flexbox and Alignment
+            'flex items-center justify-center',
+
+            // Size and Spacing
+            'mr-2',
+            'last:mr-0',
+            'w-6 h-6',
+
+            // Shape
+            'border-0',
+            'rounded-full',
+
+            // Colors
+            'text-surface-500',
+            'bg-transparent',
+
+            // Transitions
+            'transition duration-200 ease-in-out',
+
+            // States
+            'hover:text-surface-700 dark:hover:text-white/80',
+            'hover:bg-surface-100 dark:hover:bg-surface-800/80',
+            'focus:outline-none focus:outline-offset-0 focus:ring-1 focus:ring-inset',
+            'focus:ring-primary-500 dark:focus:ring-primary-400',
+
+            // Misc
+            'overflow-hidden'
         ]
     },
-    closeButtonIcon: {
-        class: 'w-4 h-4 inline-block'
+    closeicon: {
+        class: [
+            // Display
+            'inline-block',
+
+            // Size
+            'w-3',
+            'h-3'
+        ]
     },
     content: {
-        class: ['p-5 pt-0 h-full w-full', 'grow overflow-y-auto']
+        class: [
+            // Spacing and Size
+            'px-6 pb-6',
+            'mt-6',
+            'h-full',
+            'w-full',
+
+            // Growth and Overflow
+            'grow',
+            'overflow-y-auto'
+        ]
     },
     mask: ({ props }) => ({
-        class: ['flex pointer-events-auto', 'transition duration-200 z-20 transition-colors', { 'bg-black/40 backdrop-blur-sm': props.modal }]
+        class: [
+            // Transitions
+            'transition',
+            'duration-200',
+            { 'p-5': !props.position == 'full' },
+
+            // Background and Effects
+            { 'bg-surface-500/70 dark:bg-surface-700/70': props.modal, 'backdrop-blur-sm': props.modal }
+        ]
     }),
     transition: ({ props }) => {
         return props.position === 'top'
