ede518051af9ab64863cac35c2d9e863bd80815a
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-10 17:08:34 +0300

    Add inlinemessage's preset (tw)

--- presets/tailwindui/inlinemessage/index.js
+++ presets/tailwindui/inlinemessage/index.js
@@ -1,17 +1,29 @@
 export default {
     root: ({ props }) => ({
         class: [
-            'inline-flex items-center justify-center align-top',
-            'p-3 m-0 rounded-md',
+            'inline-flex items-center justify-center align-top gap-2',
+            'py-2 px-3 m-0 rounded-md',
+            'ring-1 ring-inset ring-surface-200 dark:ring-surface-700 ring-offset-0',
             {
-                'bg-blue-100 border-0 text-blue-700': props.severity == 'info',
-                'bg-green-100 border-0 text-green-700': props.severity == 'success',
-                'bg-orange-100 border-0 text-orange-700': props.severity == 'warn',
-                'bg-red-100 border-0 text-red-700': props.severity == 'error'
+                'text-blue-500 dark:text-blue-300': props.severity == 'info',
+                'text-green-500 dark:text-green-300': props.severity == 'success',
+                'text-orange-500 dark:text-orange-300': props.severity == 'warn',
+                'text-red-500 dark:text-red-300': props.severity == 'error'
             }
         ]
     }),
     icon: {
-        class: 'text-base mr-2'
+        class: [
+            // Sizing and Spacing
+            'w-4 h-4',
+            'shrink-0'
+        ]
+    },
+    text: {
+        class: [
+            // Font and Text
+            'text-sm leading-none',
+            'font-medium'
+        ]
     }
 };
