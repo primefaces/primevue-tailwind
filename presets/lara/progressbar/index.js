67141d32567a966ae610adc8deea997bdae4910d
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-10 18:51:01 +0300

    Finalize progressbar's preset (lara)

--- presets/lara/progressbar/index.js
+++ presets/lara/progressbar/index.js
@@ -1,17 +1,55 @@
 export default {
     root: {
-        class: ['overflow-hidden relative', 'border-0 h-6 bg-surface-100 rounded-md dark:bg-surface-800']
+        class: [
+            // Position and Overflow
+            'overflow-hidden',
+            'relative',
+
+            // Shape and Size
+            'border-0',
+            'h-6',
+            'rounded-md',
+
+            // Colors
+            'bg-surface-100 dark:bg-surface-700'
+        ]
     },
     value: ({ props }) => ({
         class: [
-            'border-0 m-0 bg-primary-500',
+            // Flexbox & Overflow & Position
+            { 'absolute flex items-center justify-center overflow-hidden': props.mode !== 'indeterminate' },
+
+            // Colors
+            'bg-primary-500 dark:bg-primary-400',
+
+            // Spacing & Sizing
+            'm-0',
+            { 'h-full w-0': props.mode !== 'indeterminate' },
+
+            // Shape
+            'border-0',
+
+            // Transitions
             {
-                'transition-width duration-1000 ease-in-out absolute items-center border-0 flex h-full justify-center overflow-hidden w-0': props.mode !== 'indeterminate',
-                'progressbar-value-animate before:absolute before:top-0 before:left-0 before:bottom-0 before:bg-inherit after:absolute after:top-0 after:left-0 after:bottom-0 after:bg-inherit after:delay-1000': props.mode == 'indeterminate'
+                'transition-width duration-1000 ease-in-out': props.mode !== 'indeterminate',
+                'progressbar-value-animate': props.mode == 'indeterminate'
+            },
+
+            // Before & After (indeterminate)
+            {
+                'before:absolute before:top-0 before:left-0 before:bottom-0 before:bg-inherit ': props.mode == 'indeterminate',
+                'after:absolute after:top-0 after:left-0 after:bottom-0 after:bg-inherit after:delay-1000': props.mode == 'indeterminate'
             }
         ]
     }),
     label: {
-        class: ['inline-flex', 'text-white leading-6']
+        class: [
+            // Flexbox
+            'inline-flex',
+
+            // Font and Text
+            'text-white dark:text-surface-900',
+            'leading-6'
+        ]
     }
 };
