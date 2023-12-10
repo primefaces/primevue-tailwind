f038a0651f660fb3233489361cd2457c9626bba6
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-10 22:15:48 +0300

    Add ripple docs & presets

---
+++ doc/ripple/CustomDoc.vue
@@ -0,0 +1,183 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Default styling of the animation adds a shade of white. This can easily be customized using css that changes the color of <i>p-ink</i> element.</p>
+    </DocSectionText>
+    <div class="card flex justify-center items-center gap-2">
+        <div
+            v-ripple="{
+                pt: {
+                    root: { style: 'transform: scale(0); background: rgba(75, 175, 80, 0.3)' }
+                }
+            }"
+            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center shadow-md"
+        >
+            Green
+        </div>
+        <div
+            v-ripple="{
+                pt: {
+                    root: { style: 'transform: scale(0); background: rgba(255, 193, 6, 0.3)' }
+                }
+            }"
+            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center shadow-md"
+        >
+            Orange
+        </div>
+        <div
+            v-ripple="{
+                pt: {
+                    root: { style: 'transform: scale(0); background: rgba(156, 39, 176, 0.3)' }
+                }
+            }"
+            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center shadow-md"
+        >
+            Purple
+        </div>
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                basic: `
+<div
+    v-ripple="{
+        pt: {
+            root: { style: 'background: rgba(75, 175, 80, 0.3)' }
+        }
+    }"
+    class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
+>
+    Green
+</div>
+<div
+    v-ripple="{
+        pt: {
+            root: { style: 'background: rgba(255, 193, 6, 0.3)' }
+        }
+    }"
+    class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
+>
+    Orange
+</div>
+<div
+    v-ripple="{
+        pt: {
+            root: { style: 'background: rgba(156, 39, 176, 0.3)' }
+        }
+    }"
+    class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
+>
+    Purple
+</div>
+`,
+                options: `
+<template>
+    <div class="flex justify-center items-center gap-2">
+        <div
+            v-ripple="{
+                pt: {
+                    root: { style: 'background: rgba(75, 175, 80, 0.3)' }
+                }
+            }"
+            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
+        >
+            Green
+        </div>
+        <div
+            v-ripple="{
+                pt: {
+                    root: { style: 'background: rgba(255, 193, 6, 0.3)' }
+                }
+            }"
+            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
+        >
+            Orange
+        </div>
+        <div
+            v-ripple="{
+                pt: {
+                    root: { style: 'background: rgba(156, 39, 176, 0.3)' }
+                }
+            }"
+            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
+        >
+            Purple
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  {
+    background: var(--surface-card);
+    padding: 2rem;
+    border-radius: 10px;
+    width: 110px;
+    text-align: center;
+}
+</style>
+`,
+                composition: `
+<template>
+    <div class="flex justify-center items-center gap-2">
+        <div
+            v-ripple="{
+                pt: {
+                    root: { style: 'background: rgba(75, 175, 80, 0.3)' }
+                }
+            }"
+            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
+        >
+            Green
+        </div>
+        <div
+            v-ripple="{
+                pt: {
+                    root: { style: 'background: rgba(255, 193, 6, 0.3)' }
+                }
+            }"
+            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
+        >
+            Orange
+        </div>
+        <div
+            v-ripple="{
+                pt: {
+                    root: { style: 'background: rgba(156, 39, 176, 0.3)' }
+                }
+            }"
+            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
+        >
+            Purple
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  {
+    background: var(--surface-card);
+    padding: 2rem;
+    border-radius: 10px;
+    width: 110px;
+    text-align: center;
+}
+</style>
+`
+            }
+        };
+    }
+};
+</script>
+
+<style scoped>
+.rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center {
+    background: var(--surface-card);
+    padding: 2rem;
+    border-radius: 10px;
+    width: 110px;
+    text-align: center;
+}
+</style>
---
+++ doc/ripple/DefaultDoc.vue
@@ -0,0 +1,43 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Ripple is enabled by adding add <i>p-ripple</i> class to the target and attach the directive with the v- prefix.</p>
+    </DocSectionText>
+    <div class="card flex justify-center items-center">
+        <div v-ripple class="bg-primary-500 dark:bg-primary-400 text-surface-0 dark:text-surface-900 flex select-none justify-center items-center shadow-md rounded-md p-6 font-bold">Default</div>
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                basic: `
+<div v-ripple class="bg-primary-500 dark:bg-primary-400 text-surface-0 dark:text-surface-900 flex select-none justify-center items-center shadow-md rounded-md p-6 font-bold">Default</div>
+`,
+                options: `
+<template>
+    <div class="card card-container flex justify-center items-center">
+        <div v-ripple class="bg-primary-500 dark:bg-primary-400 text-surface-0 dark:text-surface-900 flex select-none justify-center items-center shadow-md rounded-md p-6 font-bold">Default</div>
+    </div>
+</template>
+
+<script>
+<\/script>
+`,
+                composition: `
+<template>
+    <div class="card card-container flex justify-center items-center">
+        <div v-ripple class="bg-primary-500 dark:bg-primary-400 text-surface-0 dark:text-surface-900 flex select-none justify-center items-center shadow-md rounded-md p-6 font-bold">Default</div>
+    </div>
+</template>
+
+<script setup>
+<\/script>
+`
+            }
+        };
+    }
+};
+</script>
