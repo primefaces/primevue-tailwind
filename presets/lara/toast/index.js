679ce5cd65a42995bb78ee67c9bd991c5bc6be0d
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-10 18:13:54 +0300

    Add toast's preset (lara)

--- presets/lara/toast/index.js
+++ presets/lara/toast/index.js
@@ -1,35 +1,84 @@
 export default {
-    root: {
-        class: ['w-96', 'opacity-90']
-    },
+    root: ({ props }) => ({
+        class: ['w-96 rounded-md', { '-translate-x-2/4': props.position == 'top-center' || props.position == 'bottom-center' }]
+    }),
     container: ({ props }) => ({
         class: [
             'my-4 rounded-md w-full',
+            'border-solid border-0 border-l-[6px]',
+            'backdrop-blur-[10px] shadow-md',
+            console.log(props),
+            // Colors
             {
-                'bg-blue-100 border-solid border-0 border-l-4 border-blue-500 text-blue-700': props.message.severity == 'info',
-                'bg-green-100 border-solid border-0 border-l-4 border-green-500 text-green-700': props.message.severity == 'success',
-                'bg-orange-100 border-solid border-0 border-l-4 border-orange-500 text-orange-700': props.message.severity == 'warn',
-                'bg-red-100 border-solid border-0 border-l-4 border-red-500 text-red-700': props.message.severity == 'error'
+                'bg-blue-100/70 dark:bg-blue-500/20': props.message.severity == 'info',
+                'bg-green-100/70 dark:bg-green-500/20': props.message.severity == 'success',
+                'bg-orange-100/70 dark:bg-orange-500/20': props.message.severity == 'warn',
+                'bg-red-100/70 dark:bg-red-500/20': props.message.severity == 'error'
+            },
+            {
+                'border-blue-500 dark:border-blue-400': props.message.severity == 'info',
+                'border-green-500 dark:border-green-400': props.message.severity == 'success',
+                'border-orange-500 dark:border-orange-400': props.message.severity == 'warn',
+                'border-red-500 dark:border-red-400': props.message.severity == 'error'
+            },
+            {
+                'text-blue-700 dark:text-blue-300': props.message.severity == 'info',
+                'text-green-700 dark:text-green-300': props.message.severity == 'success',
+                'text-orange-700 dark:text-orange-300': props.message.severity == 'warn',
+                'text-red-700 dark:text-red-300': props.message.severity == 'error'
             }
         ]
     }),
     content: {
-        class: 'flex items-center py-5 px-7'
+        class: 'flex items-start p-4'
     },
     icon: {
-        class: ['w-6 h-6', 'text-lg mr-2']
+        class: [
+            // Sizing and Spacing
+            'w-6 h-6',
+            'text-lg leading-none mr-2 shrink-0'
+        ]
     },
     text: {
-        class: 'text-base font-normal flex flex-col flex-1 grow shrink ml-4'
+        class: [
+            // Font and Text
+            'text-base leading-none',
+            'ml-2',
+            'flex-1'
+        ]
     },
     summary: {
         class: 'font-bold block'
     },
     detail: {
-        class: 'mt-1 block'
+        class: 'mt-2 block'
     },
     closebutton: {
-        class: ['w-8 h-8 rounded-full bg-transparent transition duration-200 ease-in-out', 'ml-auto overflow-hidden relative', 'flex items-center justify-center', 'hover:bg-surface-0/30']
+        class: [
+            // Flexbox
+            'flex items-center justify-center',
+
+            // Size
+            'w-8 h-8',
+
+            // Spacing and Misc
+            'ml-auto  relative',
+
+            // Shape
+            'rounded-full',
+
+            // Colors
+            'bg-transparent',
+
+            // Transitions
+            'transition duration-200 ease-in-out',
+
+            // States
+            'hover:bg-surface-0/50 dark:hover:bg-surface-0/10',
+
+            // Misc
+            'overflow-hidden'
+        ]
     },
     transition: {
         enterFromClass: 'opacity-0 translate-x-0 translate-y-2/4 translate-z-0',
