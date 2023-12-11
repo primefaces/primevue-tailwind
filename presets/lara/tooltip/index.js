4e4a9d2d8ed69dc23b7938d8179fa5702fdde812
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-12-11 20:48:46 +0300

    Finalize tooltip presets (lara&tw)

--- presets/lara/tooltip/index.js
+++ presets/lara/tooltip/index.js
@@ -1,25 +1,45 @@
 export default {
-    root: ({ context }) => ({
+    root: ({ context, props }) => ({
         class: [
-            'absolute shadow-md',
+            // Position and Shadows
+            'absolute',
+            'shadow-md',
+            // Spacing
             {
                 'py-0 px-1': context?.right || context?.left || (!context?.right && !context?.left && !context?.top && !context?.bottom),
                 'py-1 px-0': context?.top || context?.bottom
             }
         ]
     }),
-    arrow: ({ context }) => ({
+    arrow: ({ context, props }) => ({
         class: [
-            'absolute w-0 h-0 border-transparent border-solid',
+            // Position
+
+            'absolute',
+
+            // Size
+            'w-0',
+            'h-0',
+
+            // Shape
+            'border-transparent',
+            'border-solid',
             {
-                '-m-t-1 border-y-[0.25rem] border-r-[0.25rem] border-l-0 border-r-surface-600': context?.right || (!context?.right && !context?.left && !context?.top && !context?.bottom),
-                '-m-t-1 border-y-[0.25rem] border-l-[0.25rem] border-r-0 border-l-surface-600': context?.left,
-                '-m-l-1 border-x-[0.25rem] border-t-[0.25rem] border-b-0 border-t-surface-600': context?.top,
-                '-m-l-1 border-x-[0.25rem] border-b-[0.25rem] border-t-0 border-b-surface-600': context?.bottom
+                'border-y-[0.25rem] border-r-[0.25rem] border-l-0 border-r-surface-600': context?.right || (!context?.right && !context?.left && !context?.top && !context?.bottom),
+                'border-y-[0.25rem] border-l-[0.25rem] border-r-0 border-l-surface-600': context?.left,
+                'border-x-[0.25rem] border-t-[0.25rem] border-b-0 border-t-surface-600': context?.top,
+                'border-x-[0.25rem] border-b-[0.25rem] border-t-0 border-b-surface-600': context?.bottom
+            },
+
+            // Spacing
+            {
+                '-m-t-1 ': context?.right || (!context?.right && !context?.left && !context?.top && !context?.bottom),
+                '-m-t-1': context?.left,
+                '-m-l-1': context?.top || context?.bottom
             }
         ]
     }),
     text: {
-        class: 'p-3 bg-surface-600 text-white rounded-md whitespace-pre-line break-words'
+        class: ['p-3', 'bg-surface-600 dark:bg-surface-700', 'text-white', 'leading-none', 'rounded-md', 'whitespace-pre-line', 'break-words']
     }
 };
