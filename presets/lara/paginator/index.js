e421304b44328269930da487c69f25a469c87853
Atakan <tepe_atakan@outlook.com>
2023-12-19 16:04:12 +0300

    Finalize paginator's preset (lara)

--- presets/lara/paginator/index.js
+++ presets/lara/paginator/index.js
@@ -8,10 +8,10 @@ export default {
             'px-4 py-2',
 
             // Shape
-            'border-0 rounded-md',
+            'border-0 dark:border rounded-md',
 
             // Color
-            'bg-surface-0 dark:bg-surface-900',
+            'bg-surface-0 dark:bg-surface-800',
             'text-surface-500 dark:text-white/60',
             'dark:border-surface-700'
         ]
@@ -24,16 +24,18 @@ export default {
             'inline-flex items-center justify-center',
 
             // Shape
-            'border-0 rounded-md',
+            'border-0 rounded-full dark:rounded-md',
 
             // Size
             'min-w-[3rem] h-12 m-[0.143rem]',
+            'leading-none',
 
             // Color
             'text-surface-500 dark:text-white/60',
 
             // State
             {
+                'hover:bg-surface-50 dark:hover:bg-surface-700/70': !context.disabled,
                 'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50': !context.disabled
             },
 
@@ -41,7 +43,7 @@ export default {
             'transition duration-200',
 
             // Misc
-            'user-none overflow-hidden leading-none',
+            'user-none overflow-hidden',
             { 'cursor-default pointer-events-none opacity-60': context.disabled }
         ]
     }),
@@ -53,16 +55,18 @@ export default {
             'inline-flex items-center justify-center',
 
             // Shape
-            'border-0 rounded-md',
+            'border-0 rounded-full dark:rounded-md',
 
             // Size
             'min-w-[3rem] h-12 m-[0.143rem]',
+            'leading-none',
 
             // Color
             'text-surface-500 dark:text-white/60',
 
             // State
             {
+                'hover:bg-surface-50 dark:hover:bg-surface-700/70': !context.disabled,
                 'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50': !context.disabled
             },
 
@@ -70,7 +74,7 @@ export default {
             'transition duration-200',
 
             // Misc
-            'user-none overflow-hidden leading-none',
+            'user-none overflow-hidden',
             { 'cursor-default pointer-events-none opacity-60': context.disabled }
         ]
     }),
@@ -82,16 +86,18 @@ export default {
             'inline-flex items-center justify-center',
 
             // Shape
-            'border-0 rounded-md',
+            'border-0 rounded-full dark:rounded-md',
 
             // Size
             'min-w-[3rem] h-12 m-[0.143rem]',
+            'leading-none',
 
             // Color
             'text-surface-500 dark:text-white/60',
 
             // State
             {
+                'hover:bg-surface-50 dark:hover:bg-surface-700/70': !context.disabled,
                 'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50': !context.disabled
             },
 
@@ -99,7 +105,7 @@ export default {
             'transition duration-200',
 
             // Misc
-            'user-none overflow-hidden leading-none',
+            'user-none overflow-hidden',
             { 'cursor-default pointer-events-none opacity-60': context.disabled }
         ]
     }),
@@ -111,16 +117,18 @@ export default {
             'inline-flex items-center justify-center',
 
             // Shape
-            'border-0 rounded-md',
+            'border-0 rounded-full dark:rounded-md',
 
             // Size
             'min-w-[3rem] h-12 m-[0.143rem]',
+            'leading-none',
 
             // Color
             'text-surface-500 dark:text-white/60',
 
             // State
             {
+                'hover:bg-surface-50 dark:hover:bg-surface-700/70': !context.disabled,
                 'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50': !context.disabled
             },
 
@@ -128,7 +136,7 @@ export default {
             'transition duration-200',
 
             // Misc
-            'user-none overflow-hidden leading-none',
+            'user-none overflow-hidden',
             { 'cursor-default pointer-events-none opacity-60': context.disabled }
         ]
     }),
@@ -140,19 +148,21 @@ export default {
             'inline-flex items-center justify-center',
 
             // Shape
-            'border-0 rounded-full',
+            'border-0 rounded-full dark:rounded-md',
 
             // Size
             'min-w-[3rem] h-12 m-[0.143rem]',
+            'leading-none',
 
             // Color
-            'text-surface-500 dark:text-white/60',
+            'text-surface-500 dark:text-white/80',
             {
-                'bg-primary-50 border-primary-50 text-primary-700 dark:bg-primary-300': context.active
+                'bg-primary-50 border-primary-50 dark:border-transparent text-primary-700 dark:text-surface-0 dark:bg-primary-400/30': context.active
             },
 
             // State
             {
+                'hover:bg-surface-50 dark:hover:bg-surface-700/70': !context.disabled && !context.active,
                 'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50': !context.disabled
             },
 
@@ -160,7 +170,7 @@ export default {
             'transition duration-200',
 
             // Misc
-            'user-none overflow-hidden leading-none',
+            'user-none overflow-hidden',
             { 'cursor-default pointer-events-none opacity-60': context.disabled }
         ]
     }),
@@ -174,6 +184,9 @@ export default {
                 // Shape
                 'h-12',
                 'rounded-md',
+
+                // Spacing
+                'mx-2',
 
                 // Color and Background
                 'bg-surface-0 dark:bg-surface-900',
@@ -197,7 +210,7 @@ export default {
             class: [
                 //Font
                 'font-sans',
-                'leading-none',
+                'leading-5',
 
                 // Display
                 'block',
@@ -210,7 +223,7 @@ export default {
 
                 // Sizing and Spacing
                 'w-[1%]',
-                'p-3',
+                'p-3 pr-0',
 
                 //Shape
                 'rounded-none',
