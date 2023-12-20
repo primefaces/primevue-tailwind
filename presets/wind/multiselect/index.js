aa9fdadc6cbaed70e0e9b46d01584e2d78a5377d
Atakan <tepe_atakan@outlook.com>
2023-12-21 00:16:28 +0300

    Finalize MultiSelect's preset (wind)

--- presets/wind/multiselect/index.js
+++ presets/wind/multiselect/index.js
@@ -1,10 +1,26 @@
 export default {
     root: ({ props }) => ({
         class: [
-            'inline-flex cursor-pointer select-none',
-            'bg-surface-0 dark:bg-surface-900 border border-surface-50 dark:border-surface-700  transition-colors duration-200 ease-in-out rounded-md',
+            // Display and Position
+            'inline-flex',
+            'relative',
+
+            // Shape
             'w-full md:w-80',
-            { 'opacity-60 select-none pointer-events-none cursor-default': props?.disabled }
+            'rounded-md',
+            'shadow-sm',
+
+            // Color and Background
+            'bg-surface-0 dark:bg-surface-900',
+            'ring-1 ring-inset ring-surface-300 dark:ring-surface-700',
+
+            // States
+            'focus:outline-none focus:ring-2 focus:ring-primary-600 dark:focus:ring-primary-500',
+
+            // Misc
+            'cursor-default',
+            'select-none',
+            { 'opacity-60': props.disabled, 'pointer-events-none': props.disabled }
         ]
     }),
     labelContainer: {
@@ -12,114 +28,407 @@ export default {
     },
     label: ({ props }) => ({
         class: [
-            'block overflow-hidden whitespace-nowrap cursor-pointer overflow-ellipsis',
+            'block leading-5',
+
+            props.display === 'chip' && props?.modelValue?.length > 0 ? 'py-1 px-3' : 'py-1.5 px-3',
+
+            // Color
             'text-surface-800 dark:text-white/80',
-            'p-3 transition duration-200',
-            {
-                '!p-3': props.display !== 'chip' && (props?.modelValue == null || props?.modelValue == undefined),
-                '!py-1.5 px-3': props.display == 'chip' && props?.modelValue !== null,
-                '!p-3': props.display == 'chip' && props?.modelValue == null
-            }
+
+            // Transitions
+            'transition duration-200',
+
+            // Misc
+            'overflow-hidden whitespace-nowrap cursor-pointer overflow-ellipsis'
         ]
     }),
     token: {
-        class: ['py-1 px-2 mr-2 bg-surface-300 dark:bg-surface-700 text-surface-700 dark:text-white/80 rounded-full', 'cursor-default inline-flex items-center']
+        class: [
+            // Flexbox
+            'inline-flex items-center',
+
+            // Spacing
+            'py-0.5 px-3 mr-2',
+
+            // Shape
+            'rounded-[1.14rem]',
+
+            // Colors
+            'text-surface-700 dark:text-white/70',
+            'bg-surface-200 dark:bg-surface-700'
+        ]
     },
     removeTokenIcon: {
-        class: 'ml-2'
+        class: [
+            // Shape
+            'rounded-md leading-6',
+
+            // Spacing
+            'ml-2',
+
+            // Size
+            'w-4 h-4',
+
+            // Transition
+            'transition duration-200 ease-in-out',
+
+            // Misc
+            'cursor-pointer'
+        ]
     },
     trigger: {
-        class: ['flex items-center justify-center shrink-0', 'bg-transparent text-surface-600 dark:text-white/70 w-12 rounded-tr-lg rounded-br-lg']
+        class: [
+            //Font
+            'sm:text-sm',
+
+            // Flexbox
+            'flex items-center justify-center',
+            'shrink-0',
+
+            // Color and Background
+            'bg-transparent',
+            'text-surface-500',
+
+            // Size
+            'w-12',
+
+            // Shape
+            'rounded-tr-md',
+            'rounded-br-md'
+        ]
     },
     panel: {
-        class: ['bg-surface-0 dark:bg-surface-900 text-surface-700 dark:text-white/80 border-0 rounded-md shadow-lg']
+        class: [
+            // Position
+            'absolute top-0 left-0',
+            'mt-2',
+
+            // Shape
+            'border-0',
+            'rounded-md',
+            'shadow-md',
+
+            // Color
+            'bg-surface-0 dark:bg-surface-800',
+            'text-surface-800 dark:text-white/80',
+            'ring-1 ring-inset ring-surface-300 dark:ring-surface-700'
+        ]
     },
     header: {
-        class: ['p-3 border-b border-surface-50 dark:border-surface-700 text-surface-700 dark:text-white/80 bg-surface-100 dark:bg-surface-800 rounded-t-lg', 'flex items-center justify-between']
+        class: [
+            'flex items-center justify-between',
+            // Spacing
+            'py-1.5 px-3',
+            'm-0',
+
+            //Shape
+            'border-b',
+            'rounded-tl-md',
+            'rounded-tr-md',
+
+            // Color
+            'text-surface-700 dark:text-white/80',
+            'bg-surface-50 dark:bg-surface-800',
+            'border-surface-200 dark:border-surface-700'
+        ]
     },
     headerCheckboxContainer: {
-        class: ['inline-flex cursor-pointer select-none align-bottom relative', 'mr-2', 'w-6 h-6']
+        class: [
+            'relative',
+
+            // Alignment
+            'inline-flex',
+            'align-bottom',
+
+            // Size
+            'w-4',
+            'h-4',
+
+            // Spacing
+            'mr-2',
+
+            // Misc
+            'cursor-default',
+            'select-none'
+        ]
     },
     headerCheckbox: ({ context }) => ({
         class: [
-            'flex items-center justify-center',
-            'border-2 w-6 h-6 text-surface-600 dark:text-white/70 rounded-lg transition-colors duration-200',
-            'hover:border-primary-500 focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50',
+            // Alignment
+            'flex',
+            'items-center',
+            'justify-center',
+
+            // Size
+            'w-4',
+            'h-4',
+
+            // Shape
+            'rounded',
+            'border',
+
+            // Colors
+            'text-surface-600',
             {
-                'border-surface-50 dark:border-surface-700 bg-surface-0 dark:bg-surface-900': !context?.selected,
-                'border-primary-500 bg-primary-500': context?.selected
-            }
+                'border-surface-300 bg-surface-0 dark:border-surface-700 dark:bg-surface-900': !context?.selected,
+                'border-primary-500 bg-primary-500 dark:border-primary-400 dark:bg-primary-400': context?.selected
+            },
+
+            'focus:outline-offset-0 focus:ring-2 focus:ring-inset focus:ring-primary-500 dark:focus:ring-primary-400'
         ]
     }),
     headercheckboxicon: {
-        class: 'w-4 h-4 transition-all duration-200 text-white text-base'
+        class: [
+            // Font
+            'text-normal',
+
+            // Size
+            'w-3',
+            'h-3',
+
+            // Colors
+            'text-white dark:text-surface-900',
+
+            // Transitions
+            'transition-all',
+            'duration-200'
+        ]
     },
     closeButton: {
         class: [
-            'flex items-center justify-center overflow-hidden relative',
-            'w-8 h-8 text-surface-500 dark:text-white/70 border-0 bg-transparent rounded-full transition duration-200 ease-in-out mr-2 last:mr-0',
-            'hover:text-surface-700 dark:hover:text-white/80 hover:border-transparent hover:bg-surface-100 dark:hover:bg-surface-800/80 ',
-            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50'
+            'relative',
+
+            // Flexbox and Alignment
+            'flex items-center justify-center',
+
+            // Size and Spacing
+            'mr-2',
+            'last:mr-0',
+            'w-6 h-6',
+
+            // Shape
+            'border-0',
+            'rounded-full',
+
+            // Colors
+            'text-surface-500',
+            'bg-transparent',
+
+            // Transitions
+            'transition duration-200 ease-in-out',
+
+            // States
+            'hover:text-surface-700 dark:hover:text-white/80',
+            'hover:bg-surface-100 dark:hover:bg-surface-800/80',
+            'focus:outline-none focus:outline-offset-0 focus:ring-1 focus:ring-inset',
+            'focus:ring-primary-500 dark:focus:ring-primary-400',
+
+            // Misc
+            'overflow-hidden'
         ]
     },
     closeButtonIcon: {
-        class: 'w-4 h-4 inline-block'
+        class: [
+            // Display
+            'inline-block',
+
+            // Size
+            'w-3',
+            'h-3'
+        ]
     },
     wrapper: {
-        class: ['max-h-[200px] overflow-auto', 'bg-surface-0 text-surface-700 border-0 rounded-md shadow-lg', 'dark:bg-surface-900 dark:text-white/80']
+        class: [
+            // Sizing
+            'max-h-[15rem]',
+
+            // Misc
+            'overflow-auto'
+        ]
     },
     list: {
-        class: 'py-3 list-none m-0'
+        class: 'py-1 list-none m-0'
     },
     item: ({ context }) => ({
         class: [
-            'cursor-pointer font-normal overflow-hidden relative whitespace-nowrap',
-            'm-0 p-3 border-0  transition-shadow duration-200 rounded-none',
-            {
-                'text-surface-700 hover:text-surface-700 hover:bg-surface-100 dark:text-white/80 dark:hover:bg-surface-800': !context.focused && !context.selected,
-                'bg-surface-50 text-surface-700 dark:text-white/80 dark:bg-surface-800/90 hover:text-surface-700 hover:bg-surface-100 dark:text-white/80 dark:hover:bg-surface-800': context.focused && !context.selected,
-                'bg-primary-100 text-primary-700 dark:bg-primary-400 dark:text-white/80': context.focused && context.selected,
-                'bg-primary-50 text-primary-700 dark:bg-primary-300 dark:text-white/80': !context.focused && context.selected
-            }
+            // Font
+            'sm:text-sm',
+            'leading-none',
+            { 'font-normal': !context.selected, 'font-bold': context.selected },
+
+            // Flexbox
+            'flex items-center',
+
+            // Position
+            'relative',
+
+            // Shape
+            'border-0',
+            'rounded-none',
+
+            // Spacing
+            'm-0',
+            'py-2 px-4',
+
+            // Color
+            { 'text-surface-700 dark:text-white/80': !context.focused && !context.selected },
+            { 'bg-primary-500 dark:bg-primary-400 text-white dark:text-surface-700': context.focused && context.selected },
+            { 'bg-transparent text-surface-700 dark:text-white/80': !context.focused && context.selected },
+
+            //States
+            'hover:bg-primary-500 dark:hover:bg-primary-400 hover:text-white dark:hover:text-surface-700',
+
+            // Misc
+            'cursor-pointer',
+            'overflow-hidden',
+            'whitespace-nowrap'
         ]
     }),
     checkboxContainer: {
-        class: ['inline-flex cursor-pointer select-none align-bottom relative', 'mr-2', 'w-6 h-6']
+        class: [
+            'relative',
+
+            // Alignment
+            'inline-flex',
+            'align-bottom',
+
+            // Size
+            'w-4',
+            'h-4',
+
+            // Spacing
+            'mr-2',
+
+            // Misc
+            'cursor-default',
+            'select-none'
+        ]
     },
     checkbox: ({ context }) => ({
         class: [
-            'flex items-center justify-center',
-            'border-2 w-6 h-6 text-surface-600 dark:text-white/80 rounded-lg transition-colors duration-200',
-            'hover:border-primary-500 focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50',
+            // Alignment
+            'flex',
+            'items-center',
+            'justify-center',
+
+            // Size
+            'w-4',
+            'h-4',
+
+            // Shape
+            'rounded',
+            'border',
+
+            // Colors
+            'text-surface-600',
             {
-                'border-surface-50 dark:border-surface-700  bg-surface-0 dark:bg-surface-900': !context?.selected,
-                'border-primary-500 bg-primary-500': context?.selected
-            }
+                'border-surface-300 bg-surface-0 dark:border-surface-700 dark:bg-surface-900': !context?.selected,
+                'border-primary-500 bg-primary-500 dark:border-primary-400 dark:bg-primary-400': context?.selected
+            },
+
+            'focus:outline-offset-0 focus:ring-2 focus:ring-inset focus:ring-primary-500 dark:focus:ring-primary-400'
         ]
     }),
     checkboxicon: {
-        class: 'w-4 h-4 transition-all duration-200 text-white text-base'
+        class: [
+            // Font
+            'text-normal',
+
+            // Size
+            'w-3',
+            'h-3',
+
+            // Colors
+            'text-white dark:text-surface-900',
+
+            // Transitions
+            'transition-all',
+            'duration-200'
+        ]
     },
     itemgroup: {
-        class: ['m-0 p-3 text-surface-800 bg-surface-0 font-bold', 'dark:bg-surface-900 dark:text-white/80', 'cursor-auto']
+        class: [
+            //Font
+            'font-bold',
+            'sm:text-sm',
+
+            // Spacing
+            'm-0',
+            'py-2 px-4',
+
+            // Color
+            'text-surface-800 dark:text-white/80',
+            'bg-surface-0 dark:bg-surface-600/80',
+
+            // Misc
+            'cursor-auto'
+        ]
     },
     filtercontainer: {
-        class: 'relative'
+        class: 'relative w-full mr-2'
     },
     filterinput: {
         class: [
-            'pr-7 -mr-7',
+            // Font
+            'font-sans',
+            'leading-none',
+            'sm:text-sm',
+
+            // Sizing
+            'py-1.5 px-3',
+            'pr-7',
+            '-mr-7',
             'w-full',
-            'font-sans text-base text-surface-700 bg-surface-0 py-3 px-3 border border-surface-50 transition duration-200 rounded-lg appearance-none',
-            'dark:bg-surface-900 dark:border-surface-700 dark:hover:border-primary-300 dark:text-white/80',
-            'hover:border-primary-500 focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50'
+
+            //Color
+            'text-surface-700 dark:text-white/80',
+            'bg-surface-0 dark:bg-surface-900',
+            'placeholder:text-surface-400',
+            'ring-1 ring-inset ring-surface-300 dark:ring-surface-700',
+
+            // Shape
+            'border-0',
+            'rounded-md',
+            'appearance-none',
+
+            // States
+            'focus:ring-2 focus:ring-inset focus:outline-none focus:outline-offset-0',
+            'focus:ring-primary-600 dark:focus:ring-primary-500',
+
+            // Misc
+            'appearance-none'
         ]
     },
     filtericon: {
-        class: '-mt-2 absolute top-1/2'
+        class: ['absolute', 'top-1/2', '-mt-2']
     },
     clearicon: {
-        class: 'text-surface-500 right-12 -mt-2 absolute top-1/2'
+        class: [
+            // Color
+            'text-surface-500',
+
+            // Position
+            'absolute',
+            'top-1/2',
+            'right-12',
+
+            // Spacing
+            '-mt-2'
+        ]
+    },
+    emptymessage: {
+        class: [
+            // Font
+            'leading-none',
+            'sm:text-sm',
+
+            // Spacing
+            'py-2 px-4',
+
+            // Color
+            'text-surface-800 dark:text-white/80',
+            'bg-transparent'
+        ]
     },
     transition: {
         enterFromClass: 'opacity-0 scale-y-[0.8]',
