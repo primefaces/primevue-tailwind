31c70fe6b5a3f554b11fb950e4717142c6d91f62
Atakan <tepe_atakan@outlook.com>
2023-12-26 20:27:27 +0300

    Finalize Textarea presets #31

--- presets/wind/textarea/index.js
+++ presets/wind/textarea/index.js
@@ -1,10 +1,34 @@
 export default {
     root: ({ context }) => ({
         class: [
+            // Font
+            'font-sans leading-6',
+            'sm:text-sm',
+
+            // Spacing
             'm-0',
-            'font-sans text-base text-surface-600 dark:text-white/80 bg-surface-0 dark:bg-surface-900 p-3 border border-surface-50 dark:border-surface-700 transition-colors duration-200 appearance-none rounded-lg',
-            'hover:border-primary-500 focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50',
-            { 'opacity-60 select-none pointer-events-none cursor-default': context.disabled }
+            'py-1.5 px-3',
+
+            // Shape
+            'rounded-md',
+            'appearance-none',
+
+            // Colors
+            'text-surface-900 dark:text-surface-0',
+            'placeholder:text-surface-400 dark:placeholder:text-surface-500',
+            'bg-surface-0 dark:bg-surface-900',
+            'ring-1 ring-inset ring-surface-300 dark:ring-surface-700 ring-offset-0',
+            'shadow-sm',
+
+            // States
+            {
+                'outline-none focus:ring-primary-500 dark:focus:ring-primary-400': !context.disabled,
+                'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+            },
+
+            // Misc
+            'appearance-none',
+            'transition-colors duration-200'
         ]
     })
 };
