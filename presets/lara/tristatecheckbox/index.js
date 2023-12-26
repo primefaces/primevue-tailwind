0f693f7cb77a10ee88712669f7abb55a1b08f330
Atakan <tepe_atakan@outlook.com>
2023-12-26 20:11:31 +0300

    Finalize TriStateCheckbox presets #29

--- presets/lara/tristatecheckbox/index.js
+++ presets/lara/tristatecheckbox/index.js
@@ -2,18 +2,60 @@ export default {
     root: {
         class: ['cursor-pointer inline-flex relative select-none align-bottom', 'w-6 h-6']
     },
-    checkbox: ({ props }) => ({
+    checkbox: ({ props, context }) => ({
         class: [
             'flex items-center justify-center',
             'border-2 w-6 h-6 rounded-lg transition-colors duration-200',
             {
-                'border-primary-500 bg-primary-500 text-white dark:border-primary-400 dark:bg-primary-400': props.modelValue || !props.modelValue,
-                'border-surface-50 text-surface-600 bg-surface-0 dark:border-surface-700 dark:bg-surface-900': props.modelValue == null
+                'border-surface-200 bg-surface-0 dark:border-surface-700 dark:bg-surface-900': props.modelValue == null,
+                'border-primary-500 bg-primary-500 dark:border-primary-400 dark:bg-primary-400': props.modelValue !== null
             },
+
+            // States
+            'focus:outline-none focus:outline-offset-0',
             {
-                'hover:border-primary-500 dark:hover:border-primary-400 focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 ring-inset dark:focus:ring-primary-300/50': !props.disabled,
+                'hover:border-primary-500 dark:hover:border-primary-400': !props.disabled,
+                'ring ring-primary-400/50 dark:ring-primary-300/50': !props.disabled && context.focused,
                 'cursor-default opacity-60': props.disabled
-            }
+            },
+
+            // Transitions
+            'transition-colors',
+            'duration-200'
         ]
-    })
+    }),
+    checkicon: {
+        class: [
+            // Font
+            'text-base leading-none',
+
+            // Size
+            'w-4',
+            'h-4',
+
+            // Colors
+            'text-white dark:text-surface-900',
+
+            // Transitions
+            'transition-all',
+            'duration-200'
+        ]
+    },
+    uncheckicon: {
+        class: [
+            // Font
+            'text-base leading-none',
+
+            // Size
+            'w-4',
+            'h-4',
+
+            // Colors
+            'text-white dark:text-surface-900',
+
+            // Transitions
+            'transition-all',
+            'duration-200'
+        ]
+    }
 };
--- presets/wind/tristatecheckbox/index.js
+++ presets/wind/tristatecheckbox/index.js
@@ -1,19 +1,75 @@
 export default {
     root: {
-        class: ['cursor-pointer inline-flex relative select-none align-bottom', 'w-6 h-6']
+        class: ['cursor-pointer inline-flex relative select-none align-bottom', 'w-4 h-4']
     },
-    checkbox: ({ props }) => ({
+    checkbox: ({ props, context }) => ({
         class: [
-            'flex items-center justify-center',
-            'border-2 w-6 h-6 rounded-lg transition-colors duration-200',
+            'relative',
+
+            // Alignment
+            'flex',
+            'items-center',
+            'justify-center',
+
+            // Size
+            'w-4',
+            'h-4',
+
+            // Shape
+            'rounded',
+            'border',
+
+            // Colors
+            'text-surface-600',
             {
-                'border-primary-500 bg-primary-500 text-white dark:border-primary-400 dark:bg-primary-400': props.modelValue || !props.modelValue,
-                'border-surface-50 text-surface-600 bg-surface-0 dark:border-surface-700 dark:bg-surface-900': props.modelValue == null
+                'border-surface-300 bg-surface-0 dark:border-surface-700 dark:bg-surface-900': props.modelValue == null,
+                'border-primary-500 bg-primary-500 dark:border-primary-400 dark:bg-primary-400': props.modelValue !== null
             },
+
+            // States
+            'focus:outline-none focus:outline-offset-0',
             {
-                'hover:border-primary-500 dark:hover:border-primary-400 focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 ring-inset dark:focus:ring-primary-300/50': !props.disabled,
+                'ring-2 ring-primary-500 dark:ring-primary-400': !props.disabled && context.focused,
                 'cursor-default opacity-60': props.disabled
-            }
+            },
+
+            // Transitions
+            'transition-colors',
+            'duration-200'
         ]
-    })
+    }),
+    checkicon: {
+        class: [
+            // Font
+            'text-normal',
+
+            // Size
+            'w-3',
+            'h-3',
+
+            // Colors
+            'text-white dark:text-surface-900',
+
+            // Transitions
+            'transition-all',
+            'duration-200'
+        ]
+    },
+    uncheckicon: {
+        class: [
+            // Font
+            'text-normal',
+
+            // Size
+            'w-3',
+            'h-3',
+
+            // Colors
+            'text-white dark:text-surface-900',
+
+            // Transitions
+            'transition-all',
+            'duration-200'
+        ]
+    }
 };
