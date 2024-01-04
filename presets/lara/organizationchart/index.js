1de41e77ba14414c1080793dcb186accde863255
Atakan <tepe_atakan@outlook.com>
2024-01-04 13:39:50 +0300

    Finalize OrganizationChart Presets #47

--- presets/lara/organizationchart/index.js
+++ presets/lara/organizationchart/index.js
@@ -1,41 +1,101 @@
 export default {
     table: {
-        class: 'mx-auto my-0 border-spacing-0 border-separate'
-    },
-    cell: {
-        class: 'text-center align-top py-0 px-3'
-    },
-    node: {
         class: [
-            'relative inline-block bg-surface-0 border border-surface-200 text-surface-600 p-5',
-            'dark:border-surface-700 dark:bg-surface-800 dark:text-white/80' // Dark Mode
+            // Spacing & Position
+            'mx-auto my-0',
+
+            // Table Style
+            'border-spacing-0 border-separate'
         ]
     },
+    cell: {
+        class: [
+            // Alignment
+            'text-center align-top',
+
+            // Spacing
+            'py-0 px-3'
+        ]
+    },
+    node: ({ props, context }) => ({
+        class: [
+            'relative inline-block',
+
+            // Spacing
+            'p-5',
+
+            // Shape
+            'border',
+
+            // Color
+            {
+                'text-surface-600 dark:text-white/80': !context?.selected,
+                'bg-surface-0 dark:bg-surface-800': !context?.selected,
+                'border-surface-200 dark:border-surface-700': !context?.selected,
+                'text-primary-700 dark:text-surface-0': context?.selected,
+                'bg-primary-50 dark:bg-primary-400/30': context?.selected,
+                'border-primary-200 dark:border-primary-600': context?.selected
+            },
+
+            // States
+            {
+                'hover:bg-surface-100 dark:hover:bg-surface-700': context?.selectable && !context?.selected,
+                'hover:bg-primary-100 dark:hover:bg-primary-300/30': context?.selectable && context?.selected
+            },
+
+            { 'cursor-pointer': context?.selectable }
+        ]
+    }),
     linecell: {
-        class: 'text-center align-top py-0 px-3'
+        class: [
+            // Alignment
+            'text-center align-top',
+
+            // Spacing
+            'py-0 px-3'
+        ]
     },
     linedown: {
         class: [
-            'mx-auto my-0 w-px h-[20px] bg-surface-200',
-            'dark:bg-surface-700' //Dark Mode
+            // Spacing
+            'mx-auto my-0',
+
+            // Size
+            'w-px h-[20px]',
+
+            // Color
+            'bg-surface-200 dark:bg-surface-700'
         ]
     },
     lineleft: ({ context }) => ({
         class: [
-            'text-center align-top py-0 px-3 rounded-none border-r border-surface-200',
-            'dark:border-surface-700', //Dark Mode
-            {
-                'border-t': context.lineTop
-            }
+            // Alignment
+            'text-center align-top',
+
+            // Spacing
+            'py-0 px-3',
+
+            // Shape
+            'rounded-none border-r',
+            { 'border-t': context.lineTop },
+
+            // Color
+            'border-surface-200 dark:border-surface-700'
         ]
     }),
     lineright: ({ context }) => ({
         class: [
-            'text-center align-top py-0 px-3 rounded-none',
-            'dark:border-surface-700', //Dark Mode
-            {
-                'border-t border-surface-200': context.lineTop
-            }
+            // Alignment
+            'text-center align-top',
+
+            // Spacing
+            'py-0 px-3',
+
+            // Shape
+            'rounded-none',
+
+            // Color
+            { 'border-t border-surface-200 dark:border-surface-700': context.lineTop }
         ]
     }),
     nodecell: {
@@ -43,11 +103,33 @@ export default {
     },
     nodetoggler: {
         class: [
-            'absolute bottom-[-0.75rem] left-2/4 -ml-3 w-6 h-6 bg-inherit text-inherit rounded-full z-2 cursor-pointer no-underline select-none',
-            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50' // Focus styles
+            // Position
+            'absolute bottom-[-0.75rem] left-2/4 -ml-3',
+            'z-20',
+
+            // Size
+            'w-6 h-6',
+
+            // Shape
+            'rounded-full',
+
+            // Color
+            'bg-inherit text-inherit',
+
+            // Focus
+            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50',
+
+            // Misc
+            'cursor-pointer no-underline select-none'
         ]
     },
     nodetogglericon: {
-        class: 'relative inline-block w-4 h-4'
+        class: [
+            // Position
+            'relative inline-block',
+
+            // Size
+            'w-4 h-4'
+        ]
     }
 };
--- presets/wind/organizationchart/index.js
+++ presets/wind/organizationchart/index.js
@@ -1,41 +1,103 @@
 export default {
     table: {
-        class: 'mx-auto my-0 border-spacing-0 border-separate'
-    },
-    cell: {
-        class: 'text-center align-top py-0 px-3'
-    },
-    node: {
         class: [
-            'relative inline-block bg-surface-0 border border-surface-200 text-surface-600 p-5',
-            'dark:border-surface-700 dark:bg-surface-800 dark:text-white/80' // Dark Mode
+            // Spacing & Position
+            'mx-auto my-0',
+
+            // Table Style
+            'border-spacing-0 border-separate'
         ]
     },
+    cell: {
+        class: [
+            // Alignment
+            'text-center align-top',
+
+            // Spacing
+            'py-0 px-3'
+        ]
+    },
+    node: ({ context }) => ({
+        class: [
+            'relative inline-block',
+
+            // Font
+            'text-sm leading-none',
+
+            // Spacing
+            'px-5 py-3',
+
+            // Shape
+            'border',
+            'rounded',
+
+            // Color
+            'border-surface-200 dark:border-surface-700',
+            {
+                'text-surface-600 dark:text-white/80': !context?.selected,
+                'bg-surface-0 dark:bg-surface-800': !context?.selected,
+                'text-surface-700 dark:text-surface-0': context?.selected,
+                'bg-surface-50 dark:bg-surface-700': context?.selected
+            },
+
+            // States
+            {
+                'hover:bg-surface-100 dark:hover:bg-surface-600/40': context?.selectable
+            },
+
+            { 'cursor-pointer': context?.selectable }
+        ]
+    }),
     linecell: {
-        class: 'text-center align-top py-0 px-3'
+        class: [
+            // Alignment
+            'text-center align-top',
+
+            // Spacing
+            'py-0 px-3'
+        ]
     },
     linedown: {
         class: [
-            'mx-auto my-0 w-px h-[20px] bg-surface-200',
-            'dark:bg-surface-700' //Dark Mode
+            // Spacing
+            'mx-auto my-0',
+
+            // Size
+            'w-px h-[20px]',
+
+            // Color
+            'bg-surface-200 dark:bg-surface-700'
         ]
     },
     lineleft: ({ context }) => ({
         class: [
-            'text-center align-top py-0 px-3 rounded-none border-r border-surface-200',
-            'dark:border-surface-700', //Dark Mode
-            {
-                'border-t': context.lineTop
-            }
+            // Alignment
+            'text-center align-top',
+
+            // Spacing
+            'py-0 px-3',
+
+            // Shape
+            'rounded-none border-r',
+            { 'border-t': context.lineTop },
+
+            // Color
+            'border-surface-200 dark:border-surface-700'
         ]
     }),
     lineright: ({ context }) => ({
         class: [
-            'text-center align-top py-0 px-3 rounded-none',
-            'dark:border-surface-700', //Dark Mode
-            {
-                'border-t border-surface-200': context.lineTop
-            }
+            // Alignment
+            'text-center align-top',
+
+            // Spacing
+            'py-0 px-3',
+
+            // Shape
+            'rounded-none',
+
+            // Color
+            { 'border-t border-surface-200 dark:border-surface-700': context.lineTop }
         ]
     }),
     nodecell: {
@@ -43,11 +105,33 @@ export default {
     },
     nodetoggler: {
         class: [
-            'absolute bottom-[-0.75rem] left-2/4 -ml-3 w-6 h-6 bg-inherit text-inherit rounded-full z-2 cursor-pointer no-underline select-none',
-            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50' // Focus styles
+            // Position
+            'absolute bottom-[-0.75rem] left-2/4 -ml-3',
+            'z-20',
+
+            // Size
+            'w-6 h-6',
+
+            // Shape
+            'rounded-full',
+
+            // Color
+            'bg-inherit text-inherit',
+
+            // Focus
+            'focus:outline-none focus:outline-offset-0 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400',
+
+            // Misc
+            'cursor-pointer no-underline select-none'
         ]
     },
     nodetogglericon: {
-        class: 'relative inline-block w-4 h-4'
+        class: [
+            // Position
+            'relative inline-block',
+
+            // Size
+            'w-4 h-4'
+        ]
     }
 };
