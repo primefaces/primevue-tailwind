1e58858604446acb6ec3adda225bbb54384c2c61
Cagatay Civici <cagatay.civici@gmail.com>
2024-01-04 21:07:26 +0300

    Add watcher

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,9 @@ export default defineNuxtConfig({
             pathPrefix: false
         }
     ],
+    experimental: {
+        watcher: 'chokidar'
+    },
     vite: {
         optimizeDeps: {
             disabled: true
