e207c6619f13fae1029cb81aa7b486f9cdf34d61
Marcel Kühn <120525470+marcel-kuehn@users.noreply.github.com>
2024-01-04 13:41:58 +0100

    Extend icon import docs with instructions for applications without scss

--- doc/icons/ImportDoc.vue
+++ doc/icons/ImportDoc.vue
@@ -1,6 +1,10 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p>CSS file of the icon library needs to be imported in <i>styles.scss</i> of your application.</p>
+        <p>If you are using SCSS, the CSS file of the icon library needs to be imported in <i>styles.scss</i> of your application.</p>
+    </DocSectionText>
+    <DocSectionCode :code="code" hideToggleCode importCode hideCodeSandbox hideStackBlitz />
+    <DocSectionText v-bind="$attrs">
+        <p>If you are using plain css you need to import it in your <i>index.html</i> file.</p>
     </DocSectionText>
     <DocSectionCode :code="code" hideToggleCode importCode hideCodeSandbox hideStackBlitz />
 </template>
@@ -9,8 +13,11 @@
 export default {
     data() {
         return {
-            code: {
+            scssCode: {
                 basic: "\nimport 'primeicons/primeicons.css'\n"
+            },
+            cssCode: {
+                basic: '\n<link href="/node_modules/primeicons/primeicons.css" rel="stylesheet">\n'
             }
         };
     }
