1f2401b2da44dc9d31e43e2773952af319e2ce30
Atakan <tepe_atakan@outlook.com>
2024-01-26 16:41:36 +0300

    Update TreeTable's presets for checkbox #89

--- presets/lara/treetable/index.js
+++ presets/lara/treetable/index.js
@@ -316,67 +316,108 @@ export default {
                 'cursor-col-resize'
             ]
         },
-        checkboxwrapper: {
-            class: [
-                'relative',
+        rowCheckbox: {
+            root: {
+                class: [
+                    'relative',
 
-                // Flex & Alignment
-                'inline-flex align-bottom',
+                    // Alignment
+                    'inline-flex',
+                    'align-middle',
 
-                // Size
-                'w-6 h-6',
+                    // Size
+                    'w-6',
+                    'h-6',
 
-                // Spacing
-                'mr-2',
+                    // Spacing
+                    'mr-2',
 
-                // Misc
-                'cursor-pointer select-none'
-            ]
+                    // Misc
+                    'cursor-pointer',
+                    'select-none'
+                ]
+            },
+            box: ({ props, context }) => ({
+                class: [
+                    // Alignment
+                    'flex',
+                    'items-center',
+                    'justify-center',
+
+                    // Size
+                    'w-6',
+                    'h-6',
+
+                    // Shape
+                    'rounded-md',
+                    'border-2',
+
+                    // Colors
+                    {
+                        'border-surface-200 bg-surface-0 dark:border-surface-700 dark:bg-surface-900': !context.checked,
+                        'border-primary-500 bg-primary-500 dark:border-primary-400 dark:bg-primary-400': context.checked
+                    },
+
+                    // States
+                    {
+                        'peer-hover:border-primary-500 dark:peer-hover:border-primary-400': !props.disabled && !context.checked,
+                        'peer-hover:bg-primary-700 dark:peer-hover:bg-primary-300 peer-hover:border-primary-700 dark:peer-hover:border-primary-300': !props.disabled && context.checked,
+                        'peer-focus-visible:border-primary-500 dark:peer-focus-visible:border-primary-400 peer-focus-visible:ring-2 peer-focus-visible:ring-primary-400/20 dark:peer-focus-visible:ring-primary-300/20': !props.disabled,
+                        'cursor-default opacity-60': props.disabled
+                    },
+
+                    // Transitions
+                    'transition-colors',
+                    'duration-200'
+                ]
+            }),
+            input: {
+                class: [
+                    'peer',
+
+                    // Size
+                    'w-full ',
+                    'h-full',
+
+                    // Position
+                    'absolute',
+                    'top-0 left-0',
+                    'z-10',
+
+                    // Spacing
+                    'p-0',
+                    'm-0',
+
+                    // Shape
+                    'opacity-0',
+                    'rounded-md',
+                    'outline-none',
+                    'border-2 border-surface-200 dark:border-surface-700',
+
+                    // Misc
+                    'appareance-none',
+                    'cursor-pointer'
+                ]
+            },
+            icon: {
+                class: [
+                    // Font
+                    'text-base leading-none',
+
+                    // Size
+                    'w-4',
+                    'h-4',
+
+                    // Colors
+                    'text-white dark:text-surface-900',
+
+                    // Transitions
+                    'transition-all',
+                    'duration-200'
+                ]
+            }
         },
-        checkbox: ({ context }) => ({
-            class: [
-                // Flex & Alignment
-                'flex items-center justify-center',
 
-                // Shape
-                'border-2 rounded-lg',
-
-                // Size
-                'w-6 h-6',
-
-                // Color
-                'text-surface-600',
-                {
-                    'border-surface-200 bg-surface-0 dark:border-surface-700 dark:bg-surface-900': !context.checked,
-                    'border-primary-500 bg-primary-500 dark:border-primary-400 dark:bg-primary-400': context.checked
-                },
-
-                // States
-                'focus:outline-none focus:outline-offset-0',
-                {
-                    'ring ring-primary-400/50 dark:ring-primary-300/50': context.focused
-                },
-
-                // Transition
-                'transition-colors duration-200',
-
-                // Misc
-                { 'cursor-default opacity-60': context.disabled }
-            ]
-        }),
-        checkboxicon: {
-            class: [
-                // Size
-                'w-4 h-4',
-                'text-base leading-none',
-
-                // Color
-                'text-white dark:text-surface-900',
-
-                // Transition
-                'transition-all duration-200'
-            ]
-        },
         transition: {
             enterFromClass: 'opacity-0 scale-y-[0.8]',
             enterActiveClass: 'transition-[transform,opacity] duration-[120ms] ease-[cubic-bezier(0,0,0.2,1)]',
