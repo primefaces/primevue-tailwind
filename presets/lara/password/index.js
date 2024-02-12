02272028d31089f8b7aa3caa082796f186af3995
Atakan <tepe_atakan@outlook.com>
2024-02-12 15:56:48 +0300

    Add Inputtext preset inside password [fix #95]

--- presets/lara/password/index.js
+++ presets/lara/password/index.js
@@ -61,6 +61,48 @@ export default {
     hideicon: {
         class: ['absolute top-1/2 right-3 -mt-2', 'text-surface-600 dark:text-white/70']
     },
+    input: {
+        root: ({ props, context, parent }) => ({
+            class: [
+                // Font
+                'font-sans leading-none',
+
+                // Flex
+                { 'flex-1 w-[1%]': parent.instance.$name == 'InputGroup' },
+
+                // Spacing
+                'm-0',
+                {
+                    'px-4 py-4': props.size == 'large',
+                    'px-2 py-2': props.size == 'small',
+                    'p-3': props.size == null
+                },
+
+                // Shape
+                { 'rounded-md': parent.instance.$name !== 'InputGroup' },
+                { 'first:rounded-l-md rounded-none last:rounded-r-md': parent.instance.$name == 'InputGroup' },
+                { 'border-0 border-y border-l last:border-r': parent.instance.$name == 'InputGroup' },
+                { 'first:ml-0 ml-[-1px]': parent.instance.$name == 'InputGroup' && !props.showButtons },
+
+                // Colors
+                'text-surface-600 dark:text-surface-200',
+                'placeholder:text-surface-400 dark:placeholder:text-surface-500',
+                'bg-surface-0 dark:bg-surface-900',
+                'border border-surface-300 dark:border-surface-600',
+
+                // States
+                {
+                    'hover:border-primary-500 dark:hover:border-primary-400': !context.disabled,
+                    'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-500/50 dark:focus:ring-primary-400/50 focus:z-10': !context.disabled,
+                    'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+                },
+
+                // Misc
+                'appearance-none',
+                'transition-colors duration-200'
+            ]
+        })
+    },
     transition: {
         enterFromClass: 'opacity-0 scale-y-[0.8]',
         enterActiveClass: 'transition-[transform,opacity] duration-[120ms] ease-[cubic-bezier(0,0,0.2,1)]',
