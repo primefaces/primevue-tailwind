ad91cf2df966e5a10c7048fe665edebbe1e9bce4
Atakan <tepe_atakan@outlook.com>
2024-02-23 16:50:26 +0300

    add invalid to inputmask #133

--- presets/lara/inputmask/index.js
+++ presets/lara/inputmask/index.js
@@ -1,5 +1,5 @@
 export default {
-    root: ({ context }) => ({
+    root: ({ context, props }) => ({
         class: [
             // Font
             'font-sans leading-none',
@@ -11,11 +11,16 @@ export default {
             'text-surface-600 dark:text-surface-200',
             'placeholder:text-surface-400 dark:placeholder:text-surface-500',
             'bg-surface-0 dark:bg-surface-900',
-            'border border-surface-300 dark:border-surface-600',
+
+            'border',
+            { 'border-surface-300 dark:border-surface-600': !props.invalid },
+
+            // Invalid State
+            { 'border-red-500 dark:border-red-400': props.invalid },
 
             // States
             {
-                'hover:border-primary-500 dark:hover:border-primary-400': !context.disabled,
+                'hover:border-primary-500 dark:hover:border-primary-400': !context.disabled && !props.invalid,
                 'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-500/50 dark:focus:ring-primary-400/50': !context.disabled,
                 'opacity-60 select-none pointer-events-none cursor-default': context.disabled
             },
