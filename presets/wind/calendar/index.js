b02517394a6206ee18fbbec9604ac1a484848433
Atakan <tepe_atakan@outlook.com>
2024-02-23 17:43:41 +0300

    add invalid to calendar #133

--- presets/wind/calendar/index.js
+++ presets/wind/calendar/index.js
@@ -23,7 +23,11 @@ export default {
             'text-surface-900 dark:text-surface-0',
             'placeholder:text-surface-400 dark:placeholder:text-surface-500',
             'bg-surface-0 dark:bg-surface-900',
-            'ring-1 ring-inset ring-surface-300 dark:ring-surface-700 ring-offset-0',
+            'ring-1 ring-inset ring-offset-0',
+            { 'ring-surface-300 dark:ring-surface-700': !props.invalid },
+
+            // Invalid State
+            { 'ring-red-500 dark:ring-red-400': props.invalid },
 
             // Spacing
             'm-0 py-1.5 px-3',
