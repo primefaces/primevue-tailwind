16e70e1e5318f479fb58c890693cd02a88698e63
Atakan <tepe_atakan@outlook.com>
2024-02-23 16:47:25 +0300

    add invalid to inputnumber #133

--- presets/lara/inputnumber/index.js
+++ presets/lara/inputnumber/index.js
@@ -40,14 +40,18 @@ export default {
                 { '!rounded-none': parent.instance.$parentInstance?.$name == 'InputGroup' && !parent.props.showButtons },
                 { 'border-0': parent.instance.$parentInstance?.$name == 'InputGroup' && !parent.props.showButtons },
 
-                // Colorsh
+                // Colors
                 'text-surface-600 dark:text-surface-200',
                 'placeholder:text-surface-400 dark:placeholder:text-surface-500',
                 'bg-surface-0 dark:bg-surface-900',
-                'border border-surface-300 dark:border-surface-600',
+                'border',
+                { 'border-surface-300 dark:border-surface-600': !parent.props.invalid },
+
+                // Invalid State
+                { 'border-red-500 dark:border-red-400': parent.props.invalid },
 
                 // States
-                'hover:border-primary-500 dark:hover:border-primary-400',
+                { 'hover:border-primary-500 dark:hover:border-primary-400': !parent.props.invalid },
                 'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-500/50 dark:focus:ring-primary-400/50 focus:z-10',
                 { 'opacity-60 select-none pointer-events-none cursor-default': context.disabled },
 
