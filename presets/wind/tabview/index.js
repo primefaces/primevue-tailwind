741498ad0f9164fd55fce2bdb8fafff383c2608d
Buğra Sercan Şeker <bugrasercanseker@gmail.com>
2024-03-03 13:50:42 +0300

    Fix TabView Duplicate
    The code contained duplicate lines at Line 108 and 111, and the order was incorrect. I have reorganized the declarations and corrected the duplication issue. Additionally, I replaced dark:bg-surface-800 with dark:bg-primary-400 to fix Dark Mode.

--- presets/wind/tabview/index.js
+++ presets/wind/tabview/index.js
@@ -109,8 +109,8 @@ export default {
                     'bg-surface-0 dark:bg-surface-800': parent.state.d_activeIndex !== context.index,
                     'text-surface-700 dark:text-surface-0/80': parent.state.d_activeIndex !== context.index,
 
-                    'bg-surface-0 dark:bg-surface-800': parent.state.d_activeIndex === context.index,
                     'border-primary-500 dark:border-primary-400': parent.state.d_activeIndex === context.index,
+                    'bg-surface-0 dark:bg-primary-800': parent.state.d_activeIndex === context.index,
                     'text-primary-500 dark:text-primary-400': parent.state.d_activeIndex === context.index
                 },
 
