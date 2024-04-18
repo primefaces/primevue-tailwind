7c47cee7cd09f9541b72fd009e8c56b0019021ec
Atakan <tepe_atakan@outlook.com>
2024-04-18 12:46:09 +0300

    add inputtext preset for aura #220

--- presets/aura/inputtext/index.js
+++ presets/aura/inputtext/index.js
@@ -2,7 +2,7 @@ export default {
     root: ({ props, context, parent }) => ({
         class: [
             // Font
-            'font-sans leading-none',
+            'leading-none',
 
             // Flex
             { 'flex-1 w-[1%]': parent.instance.$name == 'InputGroup' },
@@ -10,9 +10,9 @@ export default {
             // Spacing
             'm-0',
             {
-                'px-4 py-4': props.size == 'large',
-                'px-2 py-2': props.size == 'small',
-                'p-3': props.size == null
+                'py-3 px-3.5': props.size == 'large',
+                'py-1.5 px-2': props.size == 'small',
+                'py-2 px-3': props.size == null
             },
 
             // Shape
@@ -24,7 +24,7 @@ export default {
             // Colors
             'text-surface-800 dark:text-white/80',
             'placeholder:text-surface-400 dark:placeholder:text-surface-500',
-            'bg-surface-0 dark:bg-surface-900',
+            { 'bg-surface-0 dark:bg-surface-900': !context.disabled },
             'border',
             { 'border-surface-300 dark:border-surface-600': !props.invalid },
 
@@ -36,8 +36,8 @@ export default {
             // States
             {
                 'hover:border-primary-500 dark:hover:border-primary-400': !context.disabled && !props.invalid,
-                'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-500/50 dark:focus:ring-primary-400/50 focus:z-10': !context.disabled,
-                'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+                'focus:outline-none focus:outline-offset-0 focus:ring-1 focus:ring-primary-500 dark:focus:ring-primary-400 focus:z-10': !context.disabled,
+                'bg-surface-200 dark:bg-surface-700 select-none pointer-events-none cursor-default': context.disabled
             },
 
             // Filled State *for FloatLabel
