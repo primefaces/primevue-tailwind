d65d576bb1a5059274b95c60792d808c71530b2c
Atakan <tepe_atakan@outlook.com>
2024-04-22 15:59:11 +0300

    add textarea preset for aura #220

--- presets/aura/textarea/index.js
+++ presets/aura/textarea/index.js
@@ -2,30 +2,32 @@ export default {
     root: ({ context, props }) => ({
         class: [
             // Font
-            'font-sans leading-none',
+            'leading-none',
 
             // Spacing
             'm-0',
-            'p-3',
+            'py-2 px-3',
 
             // Shape
             'rounded-md',
 
             // Colors
-            'text-surface-600 dark:text-surface-200',
+            'text-surface-800 dark:text-white/80',
             'placeholder:text-surface-400 dark:placeholder:text-surface-500',
-            'bg-surface-0 dark:bg-surface-900',
+            { 'bg-surface-0 dark:bg-surface-950': !context.disabled },
             'border',
             { 'border-surface-300 dark:border-surface-600': !props.invalid },
 
             // Invalid State
+            'invalid:focus:ring-red-200',
+            'invalid:hover:border-red-500',
             { 'border-red-500 dark:border-red-400': props.invalid },
 
             // States
             {
-                'hover:border-primary-500 dark:hover:border-primary-400': !context.disabled && !props.invalid,
-                'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-500/50 dark:focus:ring-primary-400/50': !context.disabled,
-                'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+                'hover:border-surface-400 dark:hover:border-surface-600': !context.disabled && !props.invalid,
+                'focus:outline-none focus:outline-offset-0 focus:ring-1 focus:ring-primary-500 dark:focus:ring-primary-400 focus:z-10': !context.disabled,
+                'bg-surface-200 dark:bg-surface-700 select-none pointer-events-none cursor-default': context.disabled
             },
 
             // Misc
