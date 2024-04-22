f2290f17d07dd5474d8eb80b5092e8a890eff939
Atakan <tepe_atakan@outlook.com>
2024-04-22 16:29:09 +0300

    add inputswitch preset for aura #220

--- presets/aura/inputswitch/index.js
+++ presets/aura/inputswitch/index.js
@@ -2,7 +2,7 @@ export default {
     root: ({ props }) => ({
         class: [
             'inline-block relative',
-            'w-12 h-7',
+            'w-10 h-6',
             'rounded-2xl',
             {
                 'opacity-60 select-none pointer-events-none cursor-default': props.disabled
@@ -13,35 +13,34 @@ export default {
         class: [
             // Position
             'absolute top-0 left-0 right-0 bottom-0',
-            { 'before:transform before:translate-x-5': props.modelValue == props.trueValue },
+            { 'before:transform before:translate-x-4': props.modelValue == props.trueValue },
 
             // Shape
             'rounded-2xl',
 
             // Before:
             'before:absolute before:top-1/2 before:left-1',
-            'before:-mt-2.5',
-            'before:h-5 before:w-5',
+            'before:-mt-2',
+            'before:h-4 before:w-4',
             'before:rounded-full',
             'before:duration-200',
-            'before:bg-surface-0 before:dark:bg-surface-900',
+            'before:bg-surface-0 before:dark:bg-surface-500',
 
             // Colors
-            'border',
             {
-                'bg-surface-200 dark:bg-surface-700': !(props.modelValue == props.trueValue),
-                'bg-primary-500 dark:bg-primary-400': props.modelValue == props.trueValue
+                'bg-surface-300 dark:bg-surface-800': !(props.modelValue == props.trueValue) && !props.invalid,
+                'bg-primary-500 dark:bg-primary-400': props.modelValue == props.trueValue && !props.invalid,
+                'before:dark:bg-surface-950': props.modelValue == props.trueValue
             },
 
-            { 'border-transparent': !props.invalid },
-
             // Invalid State
-            { 'border-red-500 dark:border-red-400': props.invalid },
+            { 'bg-red-400 dark:bg-red-300': props.invalid },
+            { 'peer-hover:bg-red-500 dark:peer-hover:bg-red-200': props.invalid },
 
             // States
-            { 'peer-hover:bg-surface-300 dark:peer-hover:bg-surface-600 ': !(props.modelValue == props.trueValue) && !props.disabled },
-            { 'peer-hover:bg-primary-600 dark:peer-hover:bg-surface-300 ': props.modelValue == props.trueValue && !props.disabled },
-            'peer-focus-visible:ring peer-focus-visible:ring-primary-400/50 dark:peer-focus-visible:ring-primary-300/50',
+            { 'peer-hover:bg-surface-400 dark:peer-hover:bg-surface-700': !(props.modelValue == props.trueValue) && !props.disabled && !props.invalid },
+            { 'peer-hover:bg-primary-600 dark:peer-hover:bg-surface-300': props.modelValue == props.trueValue && !props.disabled && !props.invalid },
+            'peer-focus-visible:ring-1 peer-focus-visible:ring-primary-500 dark:peer-focus-visible:ring-primary-400',
 
             // Transition
             'transition-colors duration-200',
@@ -69,7 +68,7 @@ export default {
 
             // Shape
             'opacity-0',
-            'rounded-[2.5rem]',
+            'rounded-2xl',
             'outline-none',
 
             // Misc
