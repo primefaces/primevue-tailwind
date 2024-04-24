7783cf0b69f6248c3a2a31d77f0d738bd89f15ec
Atakan <tepe_atakan@outlook.com>
2024-04-24 13:32:40 +0300

    add chip preset for aura #219

--- presets/aura/chip/index.js
+++ presets/aura/chip/index.js
@@ -5,24 +5,24 @@ export default {
             'inline-flex items-center',
 
             // Spacing
-            'px-3',
+            'px-3 py-1',
 
             // Shape
             'rounded-[1.14rem]',
 
             // Colors
-            'text-surface-700 dark:text-white/70',
-            'bg-surface-200 dark:bg-surface-700'
+            'text-surface-700 dark:text-white',
+            'bg-surface-100 dark:bg-surface-700'
         ]
     },
     label: {
-        class: 'leading-6 my-1.5 mx-0'
+        class: 'leading-6 m-0'
     },
     icon: {
         class: 'leading-6 mr-2'
     },
     image: {
-        class: ['w-9 h-9 -ml-3 mr-2', 'rounded-full']
+        class: ['w-8 h-8 -ml-2 mr-2', 'rounded-full']
     },
     removeIcon: {
         class: [
@@ -30,7 +30,7 @@ export default {
             'rounded-md leading-6',
 
             // Spacing
-            'ml-2',
+            'ml-[0.375rem]',
 
             // Size
             'w-4 h-4',
