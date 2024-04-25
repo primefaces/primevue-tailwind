1a27f16a450842855a2990cd2a615433cc2a3b67
Atakan <tepe_atakan@outlook.com>
2024-04-25 06:40:24 +0300

    add overlaypanel preset for aura #218

--- presets/aura/overlaypanel/index.js
+++ presets/aura/overlaypanel/index.js
@@ -3,33 +3,22 @@ export default {
         class: [
             // Shape
             'rounded-md shadow-lg',
-            'border-0 dark:border',
 
             // Position
             'absolute left-0 top-0 mt-2',
             'z-40 transform origin-center',
 
             // Color
-            'bg-surface-0 dark:bg-surface-800',
+            'bg-surface-0 dark:bg-surface-900',
             'text-surface-700 dark:text-surface-0/80',
-            'dark:border-surface-700',
 
-            // Before: Triangle
-            'before:absolute before:-top-[9px] before:-ml-[9px] before:left-[calc(var(--overlayArrowLeft,0)+1.25rem)] z-0',
-            'before:w-0 before:h-0',
-            'before:border-transparent before:border-solid',
-            'before:border-x-[8px] before:border-[8px]',
-            'before:border-t-0 before:border-b-surface-300/10 dark:before:border-b-surface-700',
-
-            'after:absolute after:-top-2 after:-ml-[8px] after:left-[calc(var(--overlayArrowLeft,0)+1.25rem)]',
-            'after:w-0 after:h-0',
-            'after:border-transparent after:border-solid',
-            'after:border-x-[0.5rem] after:border-[0.5rem]',
-            'after:border-t-0 after:border-b-surface-0 dark:after:border-b-surface-800'
+            // Before: Arrow
+            'before:absolute before:w-0 before:-top-3 before:h-0 before:border-transparent before:border-solid before:ml-[calc(var(--overlayArrowLeft,0)+1.25rem)] before:border-x-[10px] before:border-b-[10px] before:border-t-0 before:border-b-surface-200 dark:before:border-b-surface-700',
+            'after:absolute after:w-0 after:-top-[0.54rem] after:h-0 after:border-transparent after:border-solid after:ml-[calc(var(--overlayArrowLeft,0)+1.3rem)] after:border-x-[9px] after:border-b-[8px] after:border-t-0 after:border-b-surface-0 dark:after:border-b-surface-900'
         ]
     },
     content: {
-        class: 'p-5 items-center flex'
+        class: ['p-5 items-center flex', 'rounded-lg', 'border border-surface-200 dark:border-surface-700']
     },
     transition: {
         enterFromClass: 'opacity-0 scale-y-[0.8]',
