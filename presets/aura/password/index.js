6e89764587d8ccb1a6d82d9bbfb165b882392070
Atakan <tepe_atakan@outlook.com>
2024-04-26 14:33:00 +0300

    add password preset for aura #220

--- presets/aura/password/index.js
+++ presets/aura/password/index.js
@@ -1,26 +1,20 @@
 export default {
     root: ({ props }) => ({
-        class: [
-            'inline-flex relative',
-            {
-                'opacity-60 select-none pointer-events-none cursor-default': props.disabled
-            },
-            { '[&>input]:pr-10': props.toggleMask }
-        ]
+        class: ['inline-flex relative', { '[&>input]:pr-10': props.toggleMask }]
     }),
     panel: {
         class: [
             // Spacing
-            'p-5',
+            'p-3',
 
             // Shape
-            'border-0 dark:border',
+            'border',
             'shadow-md rounded-md',
 
             // Colors
             'bg-surface-0 dark:bg-surface-900',
             'text-surface-700 dark:text-white/80',
-            'dark:border-surface-700'
+            'border-surface-200 dark:border-surface-700'
         ]
     },
     meter: {
@@ -31,10 +25,11 @@ export default {
 
             // Shape and Size
             'border-0',
-            'h-3',
+            'h-[10px]',
+            'rounded-md',
 
             // Spacing
-            'mb-2',
+            'mb-3',
 
             // Colors
             'bg-surface-100 dark:bg-surface-700'
@@ -66,7 +61,7 @@ export default {
         root: ({ props, context, parent }) => ({
             class: [
                 // Font
-                'font-sans leading-none',
+                'leading-none',
 
                 // Flex
                 { 'flex-1 w-[1%]': parent.instance.$name == 'InputGroup' },
@@ -74,11 +69,10 @@ export default {
                 // Spacing
                 'm-0',
                 {
-                    'px-4 py-4': props.size == 'large',
-                    'px-2 py-2': props.size == 'small',
-                    'p-3': props.size == null
+                    'py-3 px-3.5': props.size == 'large',
+                    'py-1.5 px-2': props.size == 'small',
+                    'py-2 px-3': props.size == null
                 },
-                'w-full',
 
                 // Shape
                 { 'rounded-md': parent.instance.$name !== 'InputGroup' },
@@ -87,21 +81,26 @@ export default {
                 { 'first:ml-0 -ml-px': parent.instance.$name == 'InputGroup' && !props.showButtons },
 
                 // Colors
-                'text-surface-600 dark:text-surface-200',
+                'text-surface-800 dark:text-white/80',
                 'placeholder:text-surface-400 dark:placeholder:text-surface-500',
-                'bg-surface-0 dark:bg-surface-900',
+                { 'bg-surface-0 dark:bg-surface-950': !context.disabled },
                 'border',
-                { 'border-surface-300 dark:border-surface-600': !parent.props.invalid },
+                { 'border-surface-300 dark:border-surface-700': !props.invalid },
 
                 // Invalid State
-                { 'border-red-500 dark:border-red-400': parent.props.invalid },
+                'invalid:focus:ring-red-200',
+                'invalid:hover:border-red-500',
+                { 'border-red-500 dark:border-red-400': props.invalid },
 
                 // States
                 {
-                    'hover:border-primary-500 dark:hover:border-primary-400': !context.disabled && !parent.props.invalid,
-                    'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-500/50 dark:focus:ring-primary-400/50 focus:z-10': !context.disabled,
-                    'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+                    'hover:border-surface-400 dark:hover:border-surface-600': !context.disabled && !props.invalid,
+                    'focus:outline-none focus:outline-offset-0 focus:ring-1 focus:ring-primary-500 dark:focus:ring-primary-400 focus:z-10': !context.disabled,
+                    'bg-surface-200 dark:bg-surface-700 select-none pointer-events-none cursor-default': context.disabled
                 },
+
+                // Filled State *for FloatLabel
+                { filled: parent.instance?.$name == 'FloatLabel' && context.filled },
 
                 // Misc
                 'appearance-none',
