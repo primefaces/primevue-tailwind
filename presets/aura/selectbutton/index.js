b69e312ae3521ed69308d471088db7e68641580b
Atakan <tepe_atakan@outlook.com>
2024-04-29 06:22:27 +0300

    add selectbutton preset for aura #220

--- presets/aura/selectbutton/index.js
+++ presets/aura/selectbutton/index.js
@@ -12,30 +12,33 @@ export default {
             'inline-flex items-center align-bottom text-center',
 
             // Spacing
-            'px-4 py-3',
+            'px-4 py-2',
 
             // Shape
-            'border border-r-0',
             'first:rounded-l-md first:rounded-tr-none first:rounded-br-none',
-            'last:border-r last:rounded-tl-none last:rounded-bl-none last:rounded-r-md',
+            'last:rounded-tl-none last:rounded-bl-none last:rounded-r-md',
+
+            // Before
+            'before:absolute before:left-1 before:top-1 before:w-[calc(100%-0.5rem)] before:h-[calc(100%-0.5rem)] before:rounded-[4px] before:z-0',
+            '[&>*]:z-10',
 
             // Color
+            'bg-surface-100 dark:bg-surface-950',
             {
-                'bg-surface-0 dark:bg-surface-900': !context.active,
-                'text-surface-700 dark:text-white/80': !context.active,
-                'border-surface-200 dark:border-surface-700': !context.active && !props.invalid,
-                'bg-primary-500 dark:bg-primary-400 border-primary-500 dark:border-primary-400 text-white dark:text-surface-900': context.active
+                'text-surface-600 dark:text-white/60 before:bg-transparent': !context.active,
+                'text-surface-800 dark:text-white/80 before:bg-surface-0 dark:before:bg-surface-800': context.active
             },
+
             // Invalid State
-            { 'border-red-500 dark:border-red-400': props.invalid },
+            { 'border first:border-r-0 last:border-l-0 border-red-500 dark:border-red-400': props.invalid },
 
             // States
-            'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-400/50 dark:focus:ring-primary-300/50 focus:z-10',
+            'focus-visible:outline-none focus-visible:outline-offset-0 focus-visible:ring-1 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 focus:z-10',
             {
-                'hover:bg-surface-50 dark:hover:bg-surface-800/80': !context.active && !props.invalid,
-                'hover:bg-primary-600 dark:hover:bg-primary-300': context.active
+                'hover:text-surface-800 dark:hover:text-white/80': !context.active && !props.invalid
             },
             { 'opacity-60 select-none pointer-events-none cursor-default': context.disabled },
+
             // Transition
             'transition duration-200',
 
@@ -44,6 +47,6 @@ export default {
         ]
     }),
     label: {
-        class: 'font-bold'
+        class: 'font-medium leading-[normal] text-center w-full z-10 relative'
     }
 };
