3bd105f1be3e1771aeefe8139a4044ba9c34475b
Baljeet Singh <bsinghs.007@gmail.com>
2024-06-06 15:35:10 +0530

    fix: add separator in menu (lara,aura)

--- presets/lara/menu/index.js
+++ presets/lara/menu/index.js
@@ -92,5 +92,11 @@ export default {
         enterActiveClass: 'transition-[transform,opacity] duration-[120ms] ease-[cubic-bezier(0,0,0.2,1)]',
         leaveActiveClass: 'transition-opacity duration-100 ease-linear',
         leaveToClass: 'opacity-0'
-    }
+    },
+    separator: {
+        class: [
+            // border
+            'my-1 border-t border-surface-200 dark:border-surface-700'
+        ]
+    },
 };
