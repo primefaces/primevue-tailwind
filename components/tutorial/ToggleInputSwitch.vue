4d853f3a35462945a8e97b1e59d944aaf2eff39a
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-07-19 16:08:39 +0300

    Doc updates

--- components/tutorial/ToggleInputSwitch.vue
+++ components/tutorial/ToggleInputSwitch.vue
@@ -16,34 +16,30 @@ const preset = {
     root: ({ props }) => ({
         class: [
             'inline-block relative',
-            'w-10 h-4',
+            'w-10 h-6',
+            'rounded-2xl',
             {
-                'opacity-40 select-none pointer-events-none cursor-default': props.disabled
+                'opacity-60 select-none pointer-events-none cursor-default': props.disabled
             }
         ]
     }),
-    slider: ({ props, state, attrs }) => ({
+    slider: ({ props, attrs }) => ({
         class: [
             // Position
             'absolute top-0 left-0 right-0 bottom-0 before:transform',
-            { 'before:translate-x-5': props.modelValue },
-            { 'before:-translate-x-1': !props.modelValue },
+            { 'before:transform before:translate-x-4': props.modelValue == props.trueValue },
 
             // Shape
             'rounded-2xl',
 
             // Before:
-            'before:absolute before:top-1/2',
-            'before:-mt-3',
-            'before:h-6 before:w-6',
+            'before:absolute before:top-1/2 before:left-1',
+            'before:-mt-2',
+            'before:h-4 before:w-4',
             'before:rounded-full',
             'before:duration-200',
-            'before:flex before:justify-center before:items-center',
-            'before:[text-shadow:0px_0px_WHITE] before:text-transparent',
-            { 'before:ring-4': state.focused },
             {
-                "before:bg-surface-500 before:dark:bg-surface-500 before:content-['➖']": !props.modelValue,
-                "before:content-['✔️']": props.modelValue,
+                'before:bg-surface-0 before:dark:bg-surface-500': !props.modelValue,
                 'before:bg-violet-500 before:ring-violet-300': !attrs.type & props.modelValue,
                 'before:bg-amber-500 before:ring-amber-300': attrs.type === 'accent' && props.modelValue
             },
@@ -51,14 +47,14 @@ const preset = {
             // Colors
             'border border-transparent',
             {
-                'bg-surface-200 dark:bg-surface-400 before:ring-surface-200 dark:before:ring-surface-400': !props.modelValue,
+                'bg-surface-300 dark:bg-surface-800': !props.modelValue,
                 'bg-violet-300': !attrs.type & props.modelValue,
                 'bg-amber-300': attrs.type === 'accent' && props.modelValue
             },
 
             // States
             {
-                'peer-hover:before:bg-surface-400 peer-hover:dark:before:bg-surface-600': !props.modelValue,
+                'peer-hover:before:bg-surface-400 peer-hover:dark:before:bg-surface-700': !props.modelValue,
                 'peer-hover:before:bg-violet-600': !attrs.type & props.modelValue,
                 'peer-hover:before:bg-amber-600': attrs.type === 'accent' && props.modelValue
             },
@@ -89,7 +85,7 @@ const preset = {
 
             // Shape
             'opacity-0',
-            'rounded-[2.5rem]',
+            'rounded-2xl',
             'outline-none',
 
             // Misc
