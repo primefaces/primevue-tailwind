3461f1bc7a3a942f2d9705b38da121201c0cc4f0
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-07-23 11:32:45 +0300

    Config updates

--- plugins/app.js
+++ plugins/app.js
@@ -5,7 +5,17 @@ import Lara from '@/presets/lara';
 
 const $appStatePlugin = {
     install: (app) => {
-        const _appState = reactive({ preset: 'Aura', darkMode: false, codeSandbox: false, sourceType: 'options-api', newsActive: false, announcement: null, storageKey: 'primevue-tailwind ' });
+        const _appState = reactive({
+            preset: 'Aura',
+            primary: 'noir',
+            surface: null,
+            darkMode: false,
+            codeSandbox: false,
+            sourceType: 'options-api',
+            newsActive: false,
+            announcement: null,
+            storageKey: 'primevue-tailwind '
+        });
 
         watch(
             () => _appState.preset,
