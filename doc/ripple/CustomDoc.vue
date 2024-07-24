6d9bb3d381b1946ff7a22e38f26d8a5798f0b807
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-07-24 13:48:22 +0300

    Ripple updates

--- doc/ripple/CustomDoc.vue
+++ doc/ripple/CustomDoc.vue
@@ -2,36 +2,14 @@
     <DocSectionText v-bind="$attrs">
         <p>Default styling of the animation adds a shade of white. This can easily be customized using css that changes the color of <i>p-ink</i> element.</p>
     </DocSectionText>
-    <div class="card flex justify-center items-center gap-2">
-        <div
-            v-ripple="{
-                pt: {
-                    root: { style: 'transform: scale(0); background: rgba(75, 175, 80, 0.3)' }
-                }
-            }"
-            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center shadow-md"
+    <div class="card flex flex-col gap-4 items-center">
+        <span
+            >Ripple option at the <span class="mx-1 h-8 w-8 rounded-border inline-flex items-center justify-center bg-primary text-primary-contrast"><i class="pi pi-palette"></i></span> configurator needs to be turned on for the demo.</span
         >
-            Green
-        </div>
-        <div
-            v-ripple="{
-                pt: {
-                    root: { style: 'transform: scale(0); background: rgba(255, 193, 6, 0.3)' }
-                }
-            }"
-            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center shadow-md"
-        >
-            Orange
-        </div>
-        <div
-            v-ripple="{
-                pt: {
-                    root: { style: 'transform: scale(0); background: rgba(156, 39, 176, 0.3)' }
-                }
-            }"
-            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center shadow-md"
-        >
-            Purple
+        <div class="flex justify-center gap-2">
+            <div v-ripple class="box" style="border: 1px solid rgba(75, 175, 80, 0.3); --p-ripple-background: rgba(75, 175, 80, 0.3)">Green</div>
+            <div v-ripple class="box" style="border: 1px solid rgba(255, 193, 6, 0.3); --p-ripple-background: rgba(255, 193, 6, 0.3)">Orange</div>
+            <div v-ripple class="box" style="border: 1px solid rgba(156, 39, 176, 0.3); --p-ripple-background: rgba(156, 39, 176, 0.3)">Purple</div>
         </div>
     </div>
     <DocSectionCode :code="code" />
@@ -43,76 +21,21 @@ export default {
         return {
             code: {
                 basic: `
-<div
-    v-ripple="{
-        pt: {
-            root: { style: 'background: rgba(75, 175, 80, 0.3)' }
-        }
-    }"
-    class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
->
-    Green
-</div>
-<div
-    v-ripple="{
-        pt: {
-            root: { style: 'background: rgba(255, 193, 6, 0.3)' }
-        }
-    }"
-    class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
->
-    Orange
-</div>
-<div
-    v-ripple="{
-        pt: {
-            root: { style: 'background: rgba(156, 39, 176, 0.3)' }
-        }
-    }"
-    class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
->
-    Purple
-</div>
+<div v-ripple class="box" style="border: 1px solid rgba(75, 175, 80, 0.3); --p-ripple-background: rgba(75, 175, 80, 0.3)">Green</div>
+<div v-ripple class="box" style="border: 1px solid rgba(255, 193, 6, 0.3); --p-ripple-background: rgba(255, 193, 6, 0.3)">Orange</div>
+<div v-ripple class="box" style="border: 1px solid rgba(156, 39, 176, 0.3); --p-ripple-background: rgba(156, 39, 176, 0.3)">Purple</div>
 `,
                 options: `
 <template>
     <div class="flex justify-center items-center gap-2">
-        <div
-            v-ripple="{
-                pt: {
-                    root: { style: 'background: rgba(75, 175, 80, 0.3)' }
-                }
-            }"
-            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
-        >
-            Green
-        </div>
-        <div
-            v-ripple="{
-                pt: {
-                    root: { style: 'background: rgba(255, 193, 6, 0.3)' }
-                }
-            }"
-            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
-        >
-            Orange
-        </div>
-        <div
-            v-ripple="{
-                pt: {
-                    root: { style: 'background: rgba(156, 39, 176, 0.3)' }
-                }
-            }"
-            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
-        >
-            Purple
-        </div>
+        <div v-ripple class="box" style="border: 1px solid rgba(75, 175, 80, 0.3); --p-ripple-background: rgba(75, 175, 80, 0.3)">Green</div>
+        <div v-ripple class="box" style="border: 1px solid rgba(255, 193, 6, 0.3); --p-ripple-background: rgba(255, 193, 6, 0.3)">Orange</div>
+        <div v-ripple class="box" style="border: 1px solid rgba(156, 39, 176, 0.3); --p-ripple-background: rgba(156, 39, 176, 0.3)">Purple</div>
     </div>
 </template>
 
 <style scoped>
-.rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  {
-    background: var(--bg-surface-0 dark:bg-surface-900);
+.box {
     padding: 2rem;
     border-radius: 10px;
     width: 110px;
@@ -123,42 +46,14 @@ export default {
                 composition: `
 <template>
     <div class="flex justify-center items-center gap-2">
-        <div
-            v-ripple="{
-                pt: {
-                    root: { style: 'background: rgba(75, 175, 80, 0.3)' }
-                }
-            }"
-            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
-        >
-            Green
-        </div>
-        <div
-            v-ripple="{
-                pt: {
-                    root: { style: 'background: rgba(255, 193, 6, 0.3)' }
-                }
-            }"
-            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
-        >
-            Orange
-        </div>
-        <div
-            v-ripple="{
-                pt: {
-                    root: { style: 'background: rgba(156, 39, 176, 0.3)' }
-                }
-            }"
-            class="rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  shadow-md"
-        >
-            Purple
-        </div>
+        <div v-ripple class="box" style="border: 1px solid rgba(75, 175, 80, 0.3); --p-ripple-background: rgba(75, 175, 80, 0.3)">Green</div>
+        <div v-ripple class="box" style="border: 1px solid rgba(255, 193, 6, 0.3); --p-ripple-background: rgba(255, 193, 6, 0.3)">Orange</div>
+        <div v-ripple class="box" style="border: 1px solid rgba(156, 39, 176, 0.3); --p-ripple-background: rgba(156, 39, 176, 0.3)">Purple</div>
     </div>
 </template>
 
 <style scoped>
-.rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center  {
-    background: var(--bg-surface-0 dark:bg-surface-900);
+.box {
     padding: 2rem;
     border-radius: 10px;
     width: 110px;
@@ -173,8 +68,7 @@ export default {
 </script>
 
 <style scoped>
-.rounded-md dark:border dark:border-surface-700 p-6 flex select-none justify-center items-center {
-    background: var(--bg-surface-0 dark: bg-surface-900);
+.box {
     padding: 2rem;
     border-radius: 10px;
     width: 110px;
