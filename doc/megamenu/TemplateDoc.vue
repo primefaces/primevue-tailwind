dc3304349f27dca0a7ff0dfed199ffe0d0fdb595
Atakan <tepe_atakan@outlook.com>
2024-09-16 17:28:00 +0300

    fix the megamenu template spacing #284

--- doc/megamenu/TemplateDoc.vue
+++ doc/megamenu/TemplateDoc.vue
@@ -20,8 +20,7 @@
             </template>
             <template #item="{ item }">
                 <a v-if="item.root" class="flex items-center cursor-pointer px-4 py-2 overflow-hidden relative font-semibold text-lg uppercase" style="border-radius: 2rem">
-                    <span :class="item.icon" />
-                    <span class="ml-2">{{ item.label }}</span>
+                    <span>{{ item.label }}</span>
                 </a>
                 <a v-else-if="!item.image" class="flex items-center p-4 cursor-pointer mb-2 gap-3">
                     <span class="inline-flex items-center justify-center rounded-full bg-primary text-primary-contrast w-12 h-12">
